4df41c2a3fd66114f16de4dba1859d7f42fd667a
Hexirp <Hexirp@users.noreply.github.com>
2020-03-27 16:22:31 +0900

    Add: Main.v を

---
+++ theories/Main.v
@@ -0,0 +1,8 @@
+Unset Elimination Schemes.
+
+Set Universe Polymorphism.
+Set Polymorphic Inductive Cumulativity.
+
+Notation "x -> y" := (forall (_ : x), y) (at level 99, right associativity, y at level 200).
+
+Inductive Unit@{i} : Type@{i} := unit : Unit.
