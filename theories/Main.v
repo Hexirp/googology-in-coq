5665b611c2bab89bd745fa8283152c5ae533ed41
Hexirp <Hexirp@users.noreply.github.com>
2020-03-28 12:32:36 +0900

    Add: Sum 型を

--- theories/Main.v
+++ theories/Main.v
@@ -3,8 +3,16 @@ Unset Elimination Schemes.
 Set Universe Polymorphism.
 Set Polymorphic Inductive Cumulativity.
 
+Set Printing Universes.
+
 Notation "x -> y" := (forall (_ : x), y) (at level 99, right associativity, y at level 200).
 
 Inductive Unit@{i} : Type@{i} := unit : Unit.
 
 Inductive Void@{i} : Type@{i} :=.
+
+Inductive Sum@{i j} (A : Type@{i}) (B : Type@{j}) : Type@{max(i,j)} :=
+  | left : A -> Sum A B
+  | right : B -> Sum A B.
+
+Print Sum.
