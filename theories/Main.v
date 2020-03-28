35c3a07fdc3809b8ce6a2faf72c3fc501ad0775a
Hexirp <Hexirp@users.noreply.github.com>
2020-03-28 11:54:27 +0900

    Update: Main.v

--- theories/Main.v
+++ theories/Main.v
@@ -3,6 +3,8 @@ Unset Elimination Schemes.
 Set Universe Polymorphism.
 Set Polymorphic Inductive Cumulativity.
 
+Notation "x -> y" := (forall (_ : x), y) (at level 99, right associativity, y at level 200).
+
 Inductive Unit@{i} : Type@{i} := unit : Unit.
 
-Notation "x -> y" := (forall (_ : x), y) (at level 99, right associativity, y at level 200).
+Inductive Void@{i} : Type@{i} :=.
