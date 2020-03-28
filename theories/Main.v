395ec0dfc4a01fe548332c9f5c06d7e5131eac64
Hexirp <Hexirp@users.noreply.github.com>
2020-03-28 17:27:10 +0900

    Update: Prod を追加する

--- theories/Main.v
+++ theories/Main.v
@@ -11,6 +11,17 @@ Inductive Unit@{i} : Type@{i} := unit : Unit.
 
 Inductive Void@{i} : Type@{i} :=.
 
+Inductive Prod@{i j} (A : Type@{i}) (B : Type@{j}) : Type@{max(i,j)} :=
+  | pair : A -> B -> Prod A B.
+
+Arguments pair {A} {B} a b.
+
+Definition fst@{i j} {A : Type@{i}} {B : Type@{j}} (x : Prod@{i j} A B) : A :=
+  match x with pair a b => a end.
+
+Definition snd@{i j} {A : Type@{i}} {B : Type@{j}} (x : Prod@{i j} A B) : B :=
+  match x with pair a b => b end.
+
 Inductive Sum@{i j} (A : Type@{i}) (B : Type@{j}) : Type@{max(i,j)} :=
   | left : A -> Sum A B
   | right : B -> Sum A B.
