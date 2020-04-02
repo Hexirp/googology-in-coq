b08a9d55df56026972190e6f86e8b0453a576c19
Hexirp <Hexirp@users.noreply.github.com>
2020-04-02 18:38:42 +0900

    Update: フォーマットを変更する

--- theories/Main.v
+++ theories/Main.v
@@ -11,37 +11,36 @@ Inductive Unit@{i} : Type@{i} := unit : Unit.
 
 Inductive Void@{i} : Type@{i} :=.
 
-Inductive Prod@{i j} (A : Type@{i}) (B : Type@{j}) : Type@{max(i,j)} :=
-  | pair : A -> B -> Prod A B.
+Inductive Prod@{i j} (A : Type@{i}) (B : Type@{j}) : Type@{max(i,j)}
+  := pair : A -> B -> Prod A B.
 
 Arguments pair {A} {B} a b.
 
-Definition fst@{i j} {A : Type@{i}} {B : Type@{j}} (x : Prod@{i j} A B) : A :=
-  match x with pair a b => a end.
+Definition fst@{i j} {A : Type@{i}} {B : Type@{j}} (x : Prod@{i j} A B) : A
+  := match x with pair a b => a end.
 
-Definition snd@{i j} {A : Type@{i}} {B : Type@{j}} (x : Prod@{i j} A B) : B :=
-  match x with pair a b => b end.
+Definition snd@{i j} {A : Type@{i}} {B : Type@{j}} (x : Prod@{i j} A B) : B
+  := match x with pair a b => b end.
 
-Inductive Sum@{i j} (A : Type@{i}) (B : Type@{j}) : Type@{max(i,j)} :=
-  | left : A -> Sum A B
-  | right : B -> Sum A B.
+Inductive Sum@{i j} (A : Type@{i}) (B : Type@{j}) : Type@{max(i,j)}
+  := left : A -> Sum A B | right : B -> Sum A B.
 
 Arguments left {A} {B} a.
 Arguments right {A} {B} b.
 
-Inductive DSum@{i j} (A : Type@{i}) (B : A -> Type@{j}) : Type@{max(i,j)} :=
-  | dpair : forall a : A, B a -> DSum A B.
+Inductive DSum@{i j} (A : Type@{i}) (B : A -> Type@{j}) : Type@{max(i,j)}
+  := dpair : forall a : A, B a -> DSum A B.
 
 Arguments dpair {A} {B} a b.
 
-Definition dfst@{i j} {A : Type@{i}} {B : A -> Type@{j}} (x : DSum@{i j} A B) : A :=
-  match x with dpair a b => a end.
+Definition dfst@{i j} {A : Type@{i}} {B : A -> Type@{j}} (x : DSum@{i j} A B) : A
+  := match x with dpair a b => a end.
 
-Definition dsnd@{i j} {A : Type@{i}} {B : A -> Type@{j}} (x : DSum@{i j} A B) : B (dfst@{i j} x) :=
-  match x with dpair a b => b end.
+Definition dsnd@{i j} {A : Type@{i}} {B : A -> Type@{j}} (x : DSum@{i j} A B) : B (dfst@{i j} x)
+  := match x with dpair a b => b end.
 
-Inductive Path@{i} (A : Type@{i}) (a : A) : A -> Type@{i} :=
-  | idpath : Path A a a.
+Inductive Path@{i} (A : Type@{i}) (a : A) : A -> Type@{i}
+  := idpath : Path A a a.
 
 Arguments Path {A} a a'.
 
@@ -59,63 +58,59 @@ Definition apply@{i j} {A : Type@{i}} {B : Type@{j}} (f : A -> B) (x : A) : B :=
 
 Definition applyD@{i j} {A : Type@{i}} {B : A -> Type@{j}} (f : forall a : A, B a) (x : A) : B x := f x.
 
-Definition absurd@{i j} {A : Type@{i}} (x : Void@{j}) : A :=
-  match x with end.
+Definition absurd@{i j} {A : Type@{i}} (x : Void@{j}) : A
+  := match x with end.
 
-Definition curry@{i j k} {A : Type@{i}} {B : Type@{j}} {C : Type@{k}} (f : Prod@{i j} A B -> C) (x : A) (y : B) : C :=
-  f (pair x y).
+Definition curry@{i j k} {A : Type@{i}} {B : Type@{j}} {C : Type@{k}} (f : Prod@{i j} A B -> C) (x : A) (y : B) : C
+  := f (pair x y).
 
-Definition uncurry@{i j k} {A : Type@{i}} {B : Type@{j}} {C : Type@{k}} (f : A -> B -> C) (x : Prod@{i j} A B) : C :=
-  match x with pair a b => f a b end.
+Definition uncurry@{i j k} {A : Type@{i}} {B : Type@{j}} {C : Type@{k}} (f : A -> B -> C) (x : Prod@{i j} A B) : C
+  := match x with pair a b => f a b end.
 
-Definition inv@{i} {A : Type@{i}} {x y : A} (p : Path@{i} x y) : Path@{i} y x :=
-  match p with idpath => idpath end.
+Definition inv@{i} {A : Type@{i}} {x y : A} (p : Path@{i} x y) : Path@{i} y x
+  := match p with idpath => idpath end.
 
-Definition conc@{i} {A : Type@{i}} {x y z : A} (p : Path@{i} x y) (q : Path@{i} y z) : Path@{i} x z :=
-  match q with idpath => match p with idpath => idpath end end.
+Definition conc@{i} {A : Type@{i}} {x y z : A} (p : Path@{i} x y) (q : Path@{i} y z) : Path@{i} x z
+  := match q with idpath => match p with idpath => idpath end end.
 
-Definition ap@{i j} {A : Type@{i}} {B : Type@{j}} (f : A -> B) {x y : A} (p : Path@{i} x y) : Path@{j} (f x) (f y) :=
-  match p with idpath => idpath end.
+Definition ap@{i j} {A : Type@{i}} {B : Type@{j}} (f : A -> B) {x y : A} (p : Path@{i} x y) : Path@{j} (f x) (f y)
+  := match p with idpath => idpath end.
 
-Inductive Bool@{i} : Type@{i} :=
-  | false : Bool
-  | true : Bool.
+Inductive Bool@{i} : Type@{i}
+  := false : Bool | true : Bool.
 
-Inductive Ordering@{i} : Type@{i} :=
-  | les : Ordering
-  | eql : Ordering
-  | grt : Ordering.
+Inductive Ordering@{i} : Type@{i}
+  := les : Ordering | eql : Ordering | grt : Ordering.
 
 Definition Rel@{i j} (A : Type@{i}) : Type@{max(i,j)} := A -> A -> Bool@{j}.
 
-Inductive Acc@{i j} {A : Type@{i}} (r : Rel@{i j} A) : A -> Type@{max(i,j)} :=
-  mkAcc : forall a : A, (forall a' : A, Path@{j} (r a' a) true -> Acc r a') -> Acc r a.
+Inductive Acc@{i j} {A : Type@{i}} (r : Rel@{i j} A) : A -> Type@{max(i,j)}
+  := mkAcc : forall a : A, (forall a' : A, Path@{j} (r a' a) true -> Acc r a') -> Acc r a.
 
-Definition WFd@{i j} {A : Type@{i}} (r : Rel@{i j} A) : Type@{max(i,j)} :=
-  forall a : A, Acc@{i j} r a.
+Definition WFd@{i j} {A : Type@{i}} (r : Rel@{i j} A) : Type@{max(i,j)}
+  := forall a : A, Acc@{i j} r a.
 
 Definition Ord@{i j} (A : Type@{i}) : Type@{max(i,j)} := A -> A -> Ordering@{j}.
 
-Definition fOrdToRef@{i j k} {A : Type@{i}} (ord : Ord@{i j} A) : Rel@{i k} A :=
-  fun x y : A => match ord x y with les => true | eql => false | grt => false end.
+Definition fOrdToRef@{i j k} {A : Type@{i}} (ord : Ord@{i j} A) : Rel@{i k} A
+  := fun x y : A => match ord x y with les => true | eql => false | grt => false end.
 
-Definition OrdCoh@{i j} {A : Type@{i}} (ord : Ord@{i j} A) : Type@{max(i,j)} :=
-  forall x y : A,
+Definition OrdCoh@{i j} {A : Type@{i}} (ord : Ord@{i j} A) : Type@{max(i,j)}
+  := forall x y : A,
     let L := Path@{j} (ord x y) les in
     let R := Path@{j} (ord y x) grt in
       Prod@{j j} (L -> R) (R -> L).
 
-Inductive Nat@{i} : Type@{i} :=
-  | zero : Nat
-  | succ : Nat -> Nat.
+Inductive Nat@{i} : Type@{i}
+  := zero : Nat | succ : Nat -> Nat.
 
-Definition natOrd@{i j} : Ord@{i j} Nat@{i} :=
-  fix natOrd (x y : Nat@{i}) {struct x} : Ordering@{j} :=
-    match x, y with
+Definition natOrd@{i j} : Ord@{i j} Nat@{i}
+  := fix r (x y : Nat@{i}) {struct x} : Ordering@{j}
+    := match x, y with
     | zero, zero => eql
     | zero, succ yp => les
     | succ xp, zero => grt
-    | succ xp, succ yp => natOrd xp yp
+    | succ xp, succ yp => r xp yp
     end.
 
 Definition natOrd_m_S_n@{i j} {m n : Nat@{i}} (p : Path@{j} (natOrd@{i j} m (succ n)) les)
