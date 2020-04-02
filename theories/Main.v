07b942aa565767a5f68d5b3008194f0c476cfa6f
Hexirp <Hexirp@users.noreply.github.com>
2020-04-02 19:05:56 +0900

    Update: 宇宙の指定を減らす

--- theories/Main.v
+++ theories/Main.v
@@ -119,8 +119,8 @@ Definition natOrd_m_S_n@{i j} {m n : Nat@{i}} (p : Path@{j} (natOrd@{i j} m (suc
     := fix r (m n : Nat@{i}) {struct m}
       : Path@{j} (natOrd@{i j} m (succ@{i} n)) les@{j} -> Sum@{j j} (Path@{j} (natOrd@{i j} m n) eql@{j}) (Path@{j} (natOrd@{i j} m n) les@{j})
       := match m, n with
-      | zero, zero => fun _ => left@{j j} idpath
-      | zero, succ np => fun _ => right@{j j} idpath
+      | zero, zero => fun _ => left idpath
+      | zero, succ np => fun _ => right idpath
       | succ mp, zero => unit
       | succ mp, succ np => unit
       end
