58ad7a0963ad4b88910a6b102475d8e184d5d8d0
Hexirp <Hexirp@users.noreply.github.com>
2020-05-31 11:15:26 +0900

    Update: Main.v -> Core.v   NaturalNumber.v -> Main.v

--- theories/NaturalNumber.v
+++
@@ -1,9 +0,0 @@
-Unset Elimination Schemes.
-
-Set Universe Polymorphism.
-Set Polymorphic Inductive Cumulativity.
-
-Set Printing Universes.
-
-Inductive Nat@{i} : Type@{i}
-  := zero : Nat | succ : Nat -> Nat.
