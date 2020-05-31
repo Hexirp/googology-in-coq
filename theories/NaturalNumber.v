0de970039ef295ff8b5dbb75df94afdae8b7d950
Hexirp <Hexirp@users.noreply.github.com>
2020-05-31 11:03:30 +0900

    Add: NaturalNumber を追加する

---
+++ theories/NaturalNumber.v
@@ -0,0 +1,9 @@
+Unset Elimination Schemes.
+
+Set Universe Polymorphism.
+Set Polymorphic Inductive Cumulativity.
+
+Set Printing Universes.
+
+Inductive Nat@{i} : Type@{i}
+  := zero : Nat | succ : Nat -> Nat.
