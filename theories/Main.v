0f2dabacef6ac5f98698069a4d4b321b4fdbf6ae
Hexirp <Hexirp@users.noreply.github.com>
2020-10-18 22:34:39 +0900

    Update: 少し進める

--- theories/Main.v
+++ theories/Main.v
@@ -1,3 +1,5 @@
+(* Run with -nois. *)
+
 Require Import GiC.Core.
 
 Unset Elimination Schemes.
@@ -9,3 +11,13 @@ Set Printing Universes.
 
 Inductive Nat@{i} : Type@{i}
   := zero : Nat | succ : Nat -> Nat.
+
+Definition pred@{i} (n : Nat@{i}) : Nat@{i} :=
+  match n with zero => zero | succ np => np end.
+
+Definition add@{i} (m n : Nat@{i}) : Nat@{i} :=
+  let
+    add_inner := fix add_inner (m n : Nat@{i}) {struct m} :=
+      match m with zero => n | succ mp => succ (add_inner mp n) end
+  in
+    add_inner m n.
