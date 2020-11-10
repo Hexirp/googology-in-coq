cc5320a1d1c2ddfdc683eb072379df177d631f3a
Hexirp <Hexirp@users.noreply.github.com>
2020-11-10 18:28:37 +0900

    Add: 結合法則を

--- theories/Main.v
+++ theories/Main.v
@@ -13,9 +13,17 @@ Set Polymorphic Inductive Cumulativity.
 Set Printing Universes.
 
 Definition conc_p_1@{i} {A : Type@{i}} {x y : A}
-  : forall p : Path@{i} x y, Path@{i} (conc@{i} p idpath) p
+  : forall p : Path@{i} x y, Path@{i} (conc p idpath) p
   := fun p => match p with idpath => idpath end.
 
 Definition conc_1_p@{i} {A : Type@{i}} {x y : A}
-  : forall p : Path@{i} x y, Path@{i} (conc@{i} idpath p) p
+  : forall p : Path@{i} x y, Path@{i} (conc idpath p) p
   := fun p => match p with idpath => idpath end.
+
+Definition conc_p_cqr@{i} {A : Type@{i}} {x y z w : A}
+  : forall (p : Path@{i} x y) (q : Path@{i} y z) (r : Path@{i} z w), Path@{i} (conc p (conc q r)) (conc (conc p q) r)
+  := fun p q r => match r with idpath => match q with idpath => match p with idpath => idpath end end end.
+
+Definition conc_cpq_r@{i} {A : Type@{i}} {x y z w : A}
+  : forall (p : Path@{i} x y) (q : Path@{i} y z) (r : Path@{i} z w), Path@{i} (conc (conc p q) r) (conc p (conc q r))
+  := fun p q r => match r with idpath => match q with idpath => match p with idpath => idpath end end end.
