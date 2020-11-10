3646413dd36824fe35ca5bde4aa862edfd3fc96e
Hexirp <Hexirp@users.noreply.github.com>
2020-11-10 18:31:18 +0900

    Add: inverse law を

--- theories/Main.v
+++ theories/Main.v
@@ -27,3 +27,11 @@ Definition conc_p_cqr@{i} {A : Type@{i}} {x y z w : A}
 Definition conc_cpq_r@{i} {A : Type@{i}} {x y z w : A}
   : forall (p : Path@{i} x y) (q : Path@{i} y z) (r : Path@{i} z w), Path@{i} (conc (conc p q) r) (conc p (conc q r))
   := fun p q r => match r with idpath => match q with idpath => match p with idpath => idpath end end end.
+
+Definition conc_p_vp@{i} {A : Type@{i}} {x y : A}
+  : forall p : Path@{i} x y, Path@{i} (conc p (inv p)) idpath
+  := fun p => match p with idpath => idpath end.
+
+Definition conc_vp_p@{i} {A : Type@{i}} {x y : A}
+  : forall p : Path@{i} x y, Path@{i} (conc (inv p) p) idpath
+  := fun p => match p with idpath => idpath end.
