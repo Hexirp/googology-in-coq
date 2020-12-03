3cde81489698bdd7aacc093fbe85f0a812d197c1
Hexirp <Hexirp@users.noreply.github.com>
2020-12-03 16:54:03 +0900

    Update: build.sh でのコンパイルを一つずつやるようにする
    "aux file name mismatch" というメッセージが出なくなるようにするため。
    一度に複数のファイルを変更しても、依存関係に従って順番にビルドされると
    いうことはなく、並行にビルドされるらしいため。 coq_makefile で
    生成した Makefile もファイルを一つずつ coqc に渡しているため。

--- build.sh
+++ build.sh
@@ -1,7 +1,6 @@
-./coqc.sh \
-  -nois \
-  -verbose \
-  -R theories/ GiC \
-  theories/Base.v \
-  theories/Function.v \
+./coqc.sh -nois -verbose -R theories/ GiC \
+  theories/Base.v
+./coqc.sh -nois -verbose -R theories/ GiC \
+  theories/Function.v
+./coqc.sh -nois -verbose -R theories/ GiC \
   theories/Main.v
