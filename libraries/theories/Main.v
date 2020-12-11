6b8763eb67afd2b846025a706830d4da09365375
Hexirp <Hexirp@users.noreply.github.com>
2020-12-11 21:15:35 +0900

    Add: apD_f_p を
    HoTT.Basics.PathGroupoids を移植するため。
    
    For: https://gitlab.com/Hexirp/googology-in-coq/-/issues/1

--- libraries/theories/Main.v
+++ libraries/theories/Main.v
@@ -4,6 +4,8 @@
 
 (** 必要なライブラリをインポートします。 *)
 Require Import GiC.Base.
+Require Import GiC.Path.Base.
+Require Import GiC.Path.Fibration.
 
 (** 帰納原理 (induction principle) を生成しないように設定します。 *)
 Unset Elimination Schemes.
@@ -20,3 +22,14 @@ Set Default Proof Mode "Classic".
 
 (** ビュレットを使用しないときにエラーになるように設定します。 *)
 Set Default Goal Selector "!".
+
+(** apD_f_p です。 *)
+(* from: https://github.com/HoTT/HoTT/blob/7b1b46057f97866a0c27678940bd1333984b79fc/theories/Basics/PathGroupoids.v#L953 *)
+Definition apD_f_p@{i j | }
+  {A : Type@{i}} {B : Type@{j}} {x y : A} (p: Path@{i} x y) (f : A -> B)
+  : Path@{j} (apD f p) (conc (trpt1_A_lam_x_B_p_u p (f x)) (ap f p)).
+Proof.
+  refine (match p with idpath => _ end).
+  cbv.
+  exact idpath.
+Defined.
