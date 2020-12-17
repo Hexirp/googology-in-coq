0a353c0651410a8dc79b8f54704ee0d78e210db7
Hexirp <Hexirp@users.noreply.github.com>
2020-12-17 21:40:37 +0900

    Update: GiC.Main から Application_D へ移動させる
    適切な箇所に置くため。

--- libraries/theories/Main.v
+++ libraries/theories/Main.v
@@ -5,7 +5,6 @@
 (** 必要なライブラリをインポートします。 *)
 Require Import GiC.Base.
 Require Import GiC.Path.Base.
-Require Import GiC.Path.Fibration.
 
 (** 帰納原理 (induction principle) を生成しないように設定します。 *)
 Unset Elimination Schemes.
@@ -22,44 +21,3 @@ Set Default Proof Mode "Classic".
 
 (** ビュレットを使用しないときにエラーになるように設定します。 *)
 Set Default Goal Selector "!".
-
-(** apD_f_p です。 *)
-(* from: https://github.com/HoTT/HoTT/blob/7b1b46057f97866a0c27678940bd1333984b79fc/theories/Basics/PathGroupoids.v#L953 *)
-Definition apD_f_p@{i j | }
-  {A : Type@{i}} {B : Type@{j}} {x y : A} (p: Path@{i} x y) (f : A -> B)
-  : Path@{j} (apD f p) (conc (trpt1_A_lam_x_B_p_u p (f x)) (ap f p)).
-Proof.
-  refine (match p with idpath => _ end).
-  cbv.
-  exact idpath.
-Defined.
-
-(** apD_comp_f_g_p です。 *)
-(* from: https://github.com/HoTT/HoTT/blob/7b1b46057f97866a0c27678940bd1333984b79fc/theories/Basics/PathGroupoids.v#L959 *)
-Definition apD_comp_f_g_p@{i0 i1 j | }
-  (A0 : Type@{i0}) (A1 : Type@{i1}) (B : A1 -> Type@{j})
-  (f : forall x : A1, B x) (g : A0 -> A1) {x x' : A0} (p : Path@{i0} x x')
-  : Path@{j}
-    (apD (fun x_ : A0 => f (g x_)) p)
-    (conc (trpt1_A0_lam_x_B_f_x_p_y A0 A1 B g p (f (g x))) (apD f (ap g p))).
-Proof.
-  refine (match p with idpath => _ end).
-  cbv.
-  exact idpath.
-Defined.
-
-(** apD_f_ap_g_p です。 *)
-(* from: https://github.com/HoTT/HoTT/blob/7b1b46057f97866a0c27678940bd1333984b79fc/theories/Basics/PathGroupoids.v#L959 *)
-Definition apD_f_ap_g_p@{i0 i1 j | }
-  (A0 : Type@{i0}) (A1 : Type@{i1}) (B : A1 -> Type@{j})
-  (f : forall x : A1, B x) (g : A0 -> A1) {x x' : A0} (p : Path@{i0} x x')
-  : Path@{j}
-    (apD f (ap g p))
-    (conc
-      (inv (trpt1_A0_lam_x_B_f_x_p_y A0 A1 B g p (f (g x))))
-      (apD (fun x_ : A0 => f (g x_)) p)).
-Proof.
-  refine (match p with idpath => _ end).
-  cbv.
-  exact idpath.
-Defined.
--- libraries/theories/Path/Application_D.v
+++ libraries/theories/Path/Application_D.v
@@ -11,6 +11,7 @@ Require Import GiC.Base.
 Require Import GiC.Path.Base.
 Require Import GiC.Path.Transposition.
 Require Import GiC.Path.Transport.
+Require Import GiC.Path.Fibration.
 
 (** 帰納原理 (induction principle) を生成しないように設定します。 *)
 Unset Elimination Schemes.
@@ -28,7 +29,7 @@ Set Default Proof Mode "Classic".
 (** ビュレットを使用しないときにエラーになるように設定します。 *)
 Set Default Goal Selector "!".
 
-(** ** 道の方を扱う定理 *)
+(** ** 道にフォーカスした定理 *)
 
 (** apD_f_idpath です。 *)
 (* from: https://github.com/HoTT/HoTT/blob/756ff79da22d0804194145db775865c11c14aa48/theories/Basics/PathGroupoids.v#L372 *)
@@ -63,6 +64,49 @@ Definition apD_f_vp@{i j | }
   : Path@{j}
     (apD f (inv p))
     (path_trpt_vp_u_v B p (f y) (f x) (inv (apD f p))).
+Proof.
+  refine (match p with idpath => _ end).
+  cbv.
+  exact idpath.
+Defined.
+
+(** ** 関数にフォーカスした定理 *)
+
+(** apD_f_p です。 *)
+(* from: https://github.com/HoTT/HoTT/blob/7b1b46057f97866a0c27678940bd1333984b79fc/theories/Basics/PathGroupoids.v#L953 *)
+Definition apD_f_p@{i j | }
+  {A : Type@{i}} {B : Type@{j}} {x y : A} (p: Path@{i} x y) (f : A -> B)
+  : Path@{j} (apD f p) (conc (trpt1_A_lam_x_B_p_u p (f x)) (ap f p)).
+Proof.
+  refine (match p with idpath => _ end).
+  cbv.
+  exact idpath.
+Defined.
+
+(** apD_comp_f_g_p です。 *)
+(* from: https://github.com/HoTT/HoTT/blob/7b1b46057f97866a0c27678940bd1333984b79fc/theories/Basics/PathGroupoids.v#L959 *)
+Definition apD_comp_f_g_p@{i0 i1 j | }
+  (A0 : Type@{i0}) (A1 : Type@{i1}) (B : A1 -> Type@{j})
+  (f : forall x : A1, B x) (g : A0 -> A1) {x x' : A0} (p : Path@{i0} x x')
+  : Path@{j}
+    (apD (fun x_ : A0 => f (g x_)) p)
+    (conc (trpt1_A0_lam_x_B_f_x_p_y A0 A1 B g p (f (g x))) (apD f (ap g p))).
+Proof.
+  refine (match p with idpath => _ end).
+  cbv.
+  exact idpath.
+Defined.
+
+(** apD_f_ap_g_p です。 *)
+(* from: https://github.com/HoTT/HoTT/blob/7b1b46057f97866a0c27678940bd1333984b79fc/theories/Basics/PathGroupoids.v#L959 *)
+Definition apD_f_ap_g_p@{i0 i1 j | }
+  (A0 : Type@{i0}) (A1 : Type@{i1}) (B : A1 -> Type@{j})
+  (f : forall x : A1, B x) (g : A0 -> A1) {x x' : A0} (p : Path@{i0} x x')
+  : Path@{j}
+    (apD f (ap g p))
+    (conc
+      (inv (trpt1_A0_lam_x_B_f_x_p_y A0 A1 B g p (f (g x))))
+      (apD (fun x_ : A0 => f (g x_)) p)).
 Proof.
   refine (match p with idpath => _ end).
   cbv.
