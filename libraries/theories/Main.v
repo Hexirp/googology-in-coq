ea3c8b760718224eea77b6ec9ba443a892f48903
Hexirp <Hexirp@users.noreply.github.com>
2020-12-30 19:05:20 +0900

    Add: conc_conc2_r_inv2_r_'conc_p_vp'_q を
    HoTT.Basics.PathGroupoids を移植するため。
    
    See also: https://gitlab.com/Hexirp/googology-in-coq/-/issues/1

--- libraries/theories/Main.v
+++ libraries/theories/Main.v
@@ -39,7 +39,8 @@ Defined.
 
 (** conc_'ap_f_cpq'_f_p_inv_p_conc_conc2_idpath_'ap_f_vp'_f_p_'conc_p_vp'_ap_f_p です。 *)
 (* from: https://github.com/HoTT/HoTT/blob/7b1b46057f97866a0c27678940bd1333984b79fc/theories/Basics/PathGroupoids.v#L999 *)
-Definition conc_'ap_f_cpq'_f_p_vp_conc_conc2_1_'ap_f_vp'_f_p_'conc_p_vp'_ap_f_p@{i j | }
+Definition
+  conc_'ap_f_cpq'_f_p_vp_conc_conc2_1_'ap_f_vp'_f_p_'conc_p_vp'_ap_f_p@{i j | }
   {A : Type@{i}} {B : Type@{j}} (f : A -> B) {x y : A} (p : Path@{i} x y)
   : Path@{j}
     (conc
@@ -54,7 +55,8 @@ Defined.
 
 (** conc_'ap_f_cpq'_f_inv_p_p_conc_conc2_'ap_f_vp'_f_p_idpath_'conc_vp_p'_ap_f_p です。 *)
 (* from: https://github.com/HoTT/HoTT/blob/7b1b46057f97866a0c27678940bd1333984b79fc/theories/Basics/PathGroupoids.v#L1006 *)
-Definition conc_'ap_f_cpq'_f_vp_p_conc_conc2_'ap_f_vp'_f_p_1_'conc_vp_p'_ap_f_p@{i j | }
+Definition
+  conc_'ap_f_cpq'_f_vp_p_conc_conc2_'ap_f_vp'_f_p_1_'conc_vp_p'_ap_f_p@{i j | }
   {A : Type@{i}} {B : Type@{j}} (f : A -> B) {x y : A} (p : Path@{i} x y)
   : Path@{j}
     (conc
@@ -62,6 +64,18 @@ Definition conc_'ap_f_cpq'_f_vp_p_conc_conc2_'ap_f_vp'_f_p_1_'conc_vp_p'_ap_f_p@
       (conc (conc2 (ap_f_vp f p) idpath) (conc_vp_p (ap f p))))
     (ap (ap f) (conc_vp_p p)).
 Proof.
+  refine (match p with idpath => _ end).
+  cbv.
+  exact idpath.
+Defined.
+
+(** conc_conc2_r_inv2_r_'conc_p_vp'_q です。 *)
+(* from: https://github.com/HoTT/HoTT/blob/7b1b46057f97866a0c27678940bd1333984b79fc/theories/Basics/PathGroupoids.v#L1013 *)
+Definition conc_conc2_r_inv2_r_'conc_p_vp'_q@{i | }
+  {A : Type@{i}} {x y : A} (p q : Path@{i} x y) (r : Path@{i} p q)
+  : Path@{i} (conc (conc2 r (inv2 r)) (conc_p_vp q)) (conc_p_vp p).
+Proof.
+  refine (match r with idpath => _ end).
   refine (match p with idpath => _ end).
   cbv.
   exact idpath.
