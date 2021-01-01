b78bb8c18351453374c580fce9ed02f6a2acb4ab
Hexirp <Hexirp@users.noreply.github.com>
2021-01-01 18:17:25 +0900

    Update: shell を単純にする
    build.sh と make_document.sh で同じファイルを指定するので、
    二度手間を減らすため。

--- libraries/build.sh
+++ libraries/build.sh
@@ -1,21 +1,6 @@
 #!/bin/bash
 
-for file in \
-  Base.v \
-  Function.v \
-  Path/Base.v \
-  Path/Function.v \
-  Path/OneDim.v \
-  Path/Transposition.v \
-  Path/Functoriality.v \
-  Path/Application_1_0.v \
-  Path/Application_1_1.v \
-  Path/Transport.v \
-  Path/Fibration.v \
-  Path/Transport_2.v \
-  Path/Application_D.v \
-  Path.v \
-  Main.v
+for file in $(./files.sh)
 do
   ./coqc.sh -nois -verbose -R theories/ GiC theories/${file}
 done
---
+++ libraries/files.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+echo \
+    Base.v \
+    Function.v \
+    Path/Base.v \
+    Path/Function.v \
+    Path/OneDim.v \
+    Path/Transposition.v \
+    Path/Functoriality.v \
+    Path/Application_1_0.v \
+    Path/Application_1_1.v \
+    Path/Transport.v \
+    Path/Fibration.v \
+    Path/Transport_2.v \
+    Path/Application_D.v \
+    Path.v \
+    Main.v
