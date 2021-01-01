124d4cdba6bf122d92a96b5cb8546bef6d227412
Hexirp <Hexirp@users.noreply.github.com>
2021-01-01 18:41:13 +0900

    Update: ファイルの情報を files.sh に纏める
    今までは build.sh と make_document.sh で別々に処理対象のソースコードが
    入ったファイルを指定していた。しかし、この二つの対象は同じであり、
    新しいファイルを追加するたびに build,sh と make_document.sh で
    同じような編集を行うという労力が掛かっていた。
    
    files.sh にコンパイルなどの対象となるファイルを集めて、労力を減らす
    ことを目的とする。
    
    [1] にあるような _CoqProject ファイルを使った方法を使えば、
    files.sh は不要であるが、 make を使う必要があるなど、導入に対する
    ハードルが高く、この方法は取らなかった。
    
    [1]: https://coq.inria.fr/refman/practical-tools/utilities.html#building-a-coq-project-with-coq-makefile
    [1/archive]: https://web.archive.org/web/20201104134303/https://coq.inria.fr/refman/practical-tools/utilities.html#building-a-coq-project-with-coq-makefile

---
+++ libraries/files.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+for file in \
+  Base.v \
+  Function.v \
+  Path/Base.v \
+  Path/Function.v \
+  Path/OneDim.v \
+  Path/Transposition.v \
+  Path/Functoriality.v \
+  Path/Application_1_0.v \
+  Path/Application_1_1.v \
+  Path/Transport.v \
+  Path/Fibration.v \
+  Path/Transport_2.v \
+  Path/Application_D.v \
+  Path.v \
+  Main.v
+do
+  echo -n "theories/${file} "
+done
