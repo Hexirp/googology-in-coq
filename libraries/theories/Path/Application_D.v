73c032104c9391bb430d86952e6e9a2c63542152
Hexirp <Hexirp@users.noreply.github.com>
2021-01-02 13:17:35 +0900

    Update: GiC.Path.Application_D のドキュメントの中のソースコードを quote する
    ドキュメントを読みやすくするため。

--- libraries/theories/Path/Application_D.v
+++ libraries/theories/Path/Application_D.v
@@ -38,7 +38,7 @@ Set Default Goal Selector "!".
 
 (** ** 道にフォーカスした定理 *)
 
-(** apD_f_idpath です。 *)
+(** [apD f idpath] です。 *)
 (* from: https://github.com/HoTT/HoTT/blob/756ff79da22d0804194145db775865c11c14aa48/theories/Basics/PathGroupoids.v#L372 *)
 Definition apD_f_1@{i j | }
   {A : Type@{i}} {B : A -> Type@{j}} (f : forall x : A, B x) (x : A)
@@ -48,7 +48,7 @@ Proof.
   exact idpath.
 Defined.
 
-(** apD_f_conc_p_q です。 *)
+(** [apD f (conc p q)] です。 *)
 (* from: https://github.com/HoTT/HoTT/blob/7b1b46057f97866a0c27678940bd1333984b79fc/theories/Basics/PathGroupoids.v#L860 *)
 Definition apD_f_cpq@{i j | }
   {A : Type@{i}} {B : A -> Type@{j}} (f : forall x : A, B x)
@@ -63,7 +63,7 @@ Proof.
   exact idpath.
 Defined.
 
-(** apD_f_inv_p です。 *)
+(** [apD f (inv p)] です。 *)
 (* from: https://github.com/HoTT/HoTT/blob/7b1b46057f97866a0c27678940bd1333984b79fc/theories/Basics/PathGroupoids.v#L868 *)
 Definition apD_f_vp@{i j | }
   {A : Type@{i}} {B : A -> Type@{j}} (f : forall x : A, B x)
@@ -79,7 +79,7 @@ Defined.
 
 (** ** 関数にフォーカスした定理 *)
 
-(** apD_f_p です。 *)
+(** [apD f p] です。 *)
 (* from: https://github.com/HoTT/HoTT/blob/7b1b46057f97866a0c27678940bd1333984b79fc/theories/Basics/PathGroupoids.v#L953 *)
 Definition apD_f_p@{i j | }
   {A : Type@{i}} {B : Type@{j}} {x y : A} (p: Path@{i} x y) (f : A -> B)
@@ -90,7 +90,7 @@ Proof.
   exact idpath.
 Defined.
 
-(** apD_comp_f_g_p です。 *)
+(** [apD (comp f g) p] です。 *)
 (* from: https://github.com/HoTT/HoTT/blob/7b1b46057f97866a0c27678940bd1333984b79fc/theories/Basics/PathGroupoids.v#L959 *)
 Definition apD_comp_f_g_p@{i0 i1 j | }
   (A0 : Type@{i0}) (A1 : Type@{i1}) (B : A1 -> Type@{j})
@@ -104,8 +104,8 @@ Proof.
   exact idpath.
 Defined.
 
-(** apD_f_ap_g_p です。 *)
 (* from: https://github.com/HoTT/HoTT/blob/7b1b46057f97866a0c27678940bd1333984b79fc/theories/Basics/PathGroupoids.v#L959 *)
+(** [apD f (ap g p)] です。 *)
 Definition apD_f_ap_g_p@{i0 i1 j | }
   (A0 : Type@{i0}) (A1 : Type@{i1}) (B : A1 -> Type@{j})
   (f : forall x : A1, B x) (g : A0 -> A1) {x x' : A0} (p : Path@{i0} x x')
