d553dcac68ce151ac932653fb0fe8c5e1dd37200
Hexirp <Hexirp@users.noreply.github.com>
2021-01-02 18:24:25 +0900

    Update: wiskering についての関数の名前を変更する
    被りが多くなるため、情報を増やした方がよいため。

--- libraries/theories/Main.v
+++ libraries/theories/Main.v
@@ -97,16 +97,16 @@ Defined.
 
 (** ** 髭付け (wiskering) *)
 
-(** [Path (conc p q) (p r)] です。 *)
+(** [Path q r -> Path (conc p q) (p r)] です。 *)
 (* from: https://github.com/HoTT/HoTT/blob/7b1b46057f97866a0c27678940bd1333984b79fc/theories/Basics/PathGroupoids.v#L1027 *)
-Definition path_cpq_cpr@{i | }
+Definition fun_Path_q_r_Path_cpq_cpr@{i | }
   {A : Type@{i}} {x y z : A} (p : Path@{i} x y) {q r : Path@{i} y z}
   : Path@{i} q r -> Path@{i} (conc p q) (conc p r)
   := fun h => conc2 idpath h.
 
-(** [Path (conc p r) (q r)] です。 *)
+(** [Path p q -> Path (conc p r) (q r)] です。 *)
 (* from: https://github.com/HoTT/HoTT/blob/7b1b46057f97866a0c27678940bd1333984b79fc/theories/Basics/PathGroupoids.v#L1031 *)
-Definition path_cpr_cqr@{i | }
+Definition fun_Path_p_q_Path_cpr_cqr@{i | }
   {A : Type@{i}} {x y z : A} {p q : Path@{i} x y} (r : Path@{i} y z)
   : Path@{i} p q -> Path@{i} (conc p r) (conc q r)
   := fun h => conc2 h idpath.
