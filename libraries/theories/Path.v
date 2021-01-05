1f3e1893697dabe03e6f4f3e89d0926b0779de16
Hexirp <Hexirp@users.noreply.github.com>
2021-01-05 14:25:41 +0900

    Update: GiC.Path を
    Require を独立させるようにコーディングスタイルが変わったので、
    それに合わせるため。 GiC.Path をなんでかんでもエクスポートする
    ライブラリにしたくないため。

--- libraries/theories/Path.v
+++ libraries/theories/Path.v
@@ -4,15 +4,24 @@
 
 (** [GiC.Path] は、道に関する定義や、道だけが主体となる定理を提供します。
 
-    具体的には、このライブラリ以下にある道に関する様々なライブラリをエクスポートします。
+    具体的には、このライブラリ以下にある主要なライブラリをエクスポートします。
  *)
 
+(** 必要なライブラリを要求します。 *)
+Require GiC.Path.Base.
+Require GiC.Path.Function.
+Require GiC.Path.OneDim.
+Require GiC.Path.Transposition.
+Require GiC.Path.Functoriality.
+Require GiC.Path.Application_1_0.
+Require GiC.Path.Application_1_1.
+Require GiC.Path.Transport.
+Require GiC.Path.Fibration.
+Require GiC.Path.Transport_2.
+Require GiC.Path.Application_D.
+
 (** ライブラリをエクスポートします。 *)
-Require Export GiC.Path.Base.
-Require Export GiC.Path.Function.
-Require Export GiC.Path.OneDim.
-Require Export GiC.Path.Transposition.
-Require Export GiC.Path.Functoriality.
-Require Export GiC.Path.Application_1_0.
-Require Export GiC.Path.Application_1_1.
-Require Export GiC.Path.Transport.
+Export GiC.Path.Base.
+Export GiC.Path.OneDim.
+Export GiC.Path.Transposition.
+Export GiC.Path.Functoriality.
