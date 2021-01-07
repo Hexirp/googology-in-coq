def4fe885f0d2b0a3d5d7398ebbc588cbdcab846
Hexirp <Hexirp@users.noreply.github.com>
2021-01-07 16:39:00 +0900

    Update: Path.OneDim を
    コーディングスタイルの変更に合わせるため。 [1] で変更するのを忘れた
    ため。
    
    [1]: 2be0ad9934218720ddd6954f0c9ea6929399c003

--- libraries/theories/Path/OneDim.v
+++ libraries/theories/Path/OneDim.v
@@ -1,37 +1,41 @@
 (* Run with -nois. *)
-
-(** * GiC.Path.OneDim *)
-
 (** [GiC.Path.OneDim] は、ある型とその上の道が一次元の亜群の構造として見做せることに関する定理を提供します。
 
     具体的には、任意の型 [A] と [Path A] が [idpath] と [conc] と [inv] によって亜群になることに由来する、ある二つの 1-道の間に道が存在するという形式の定理を示しています。
  *)
 
 (** 必要なライブラリを要求します。 *)
+
 Require GiC.Base.
 
 (** 必要なライブラリをインポートします。 *)
+
 Import GiC.Base.
 
 (** 帰納原理 (induction principle) を生成しないように設定します。 *)
+
 Unset Elimination Schemes.
 
 (** 宇宙多相 (universe polymorphism) について設定します。 *)
+
 Set Universe Polymorphism.
 Set Polymorphic Inductive Cumulativity.
 
 (** 宇宙 (universe) について表示するように設定します。 *)
+
 Set Printing Universes.
 
 (** ** [idpath] の [conc] においての単位元性 *)
 
 (** [conc p idpath] です。 *)
+
 (* from: https://github.com/HoTT/HoTT/blob/756ff79da22d0804194145db775865c11c14aa48/theories/Basics/PathGroupoids.v#L70 *)
 Definition conc_p_1@{i | } {A : Type@{i}} {x y : A}
   : forall p : Path@{i} x y, Path@{i} (conc p idpath) p
   := fun p => match p with idpath => idpath end.
 
 (** [conc idpath p] です。 *)
+
 (* from: https://github.com/HoTT/HoTT/blob/756ff79da22d0804194145db775865c11c14aa48/theories/Basics/PathGroupoids.v#L76 *)
 Definition conc_1_p@{i | } {A : Type@{i}} {x y : A}
   : forall p : Path@{i} x y, Path@{i} (conc idpath p) p
@@ -40,6 +44,7 @@ Definition conc_1_p@{i | } {A : Type@{i}} {x y : A}
 (** ** [conc] の結合法則 *)
 
 (** [conc p (conc q r)] です。 *)
+
 (* from: https://github.com/HoTT/HoTT/blob/756ff79da22d0804194145db775865c11c14aa48/theories/Basics/PathGroupoids.v#L82 *)
 Definition conc_p_cqr@{i | } {A : Type@{i}} {x y z w : A}
   : forall (p : Path@{i} x y) (q : Path@{i} y z) (r : Path@{i} z w),
@@ -53,6 +58,7 @@ Definition conc_p_cqr@{i | } {A : Type@{i}} {x y z w : A}
   end.
 
 (** [conc (conc p q) r] です。 *)
+
 (* from: https://github.com/HoTT/HoTT/blob/756ff79da22d0804194145db775865c11c14aa48/theories/Basics/PathGroupoids.v#L89 *)
 Definition conc_cpq_r@{i | } {A : Type@{i}} {x y z w : A}
   : forall (p : Path@{i} x y) (q : Path@{i} y z) (r : Path@{i} z w),
@@ -68,12 +74,14 @@ Definition conc_cpq_r@{i | } {A : Type@{i}} {x y z w : A}
 (** ** [inv] の [conc] においての逆元性 *)
 
 (** [conc p (inv p)] です。 *)
+
 (* from: https://github.com/HoTT/HoTT/blob/756ff79da22d0804194145db775865c11c14aa48/theories/Basics/PathGroupoids.v#L97 *)
 Definition conc_p_vp@{i | } {A : Type@{i}} {x y : A}
   : forall p : Path@{i} x y, Path@{i} (conc p (inv p)) idpath
   := fun p => match p with idpath => idpath end.
 
 (** [conc (inv p) p] です。 *)
+
 (* from: https://github.com/HoTT/HoTT/blob/756ff79da22d0804194145db775865c11c14aa48/theories/Basics/PathGroupoids.v#L103 *)
 Definition conc_vp_p@{i | } {A : Type@{i}} {x y : A}
   : forall p : Path@{i} x y, Path@{i} (conc (inv p) p) idpath
@@ -82,6 +90,7 @@ Definition conc_vp_p@{i | } {A : Type@{i}} {x y : A}
 (** ** 結合法則と逆元 *)
 
 (** [conc (inv p) (conc p q)] です。 *)
+
 (* from: https://github.com/HoTT/HoTT/blob/756ff79da22d0804194145db775865c11c14aa48/theories/Basics/PathGroupoids.v#L110 *)
 Definition conc_vp_cpq@{i | } {A : Type@{i}} {x y z : A}
   : forall (p : Path@{i} x y) (q : Path@{i} y z),
@@ -89,6 +98,7 @@ Definition conc_vp_cpq@{i | } {A : Type@{i}} {x y z : A}
   := fun p q => match q with idpath => match p with idpath => idpath end end.
 
 (** [conc p (conc (inv p) q)] です。 *)
+
 (* from: https://github.com/HoTT/HoTT/blob/756ff79da22d0804194145db775865c11c14aa48/theories/Basics/PathGroupoids.v#L117 *)
 Definition conc_p_cvpq@{i | } {A : Type@{i}} {x y z : A}
   : forall (p : Path@{i} x y) (q : Path@{i} x z),
@@ -96,6 +106,7 @@ Definition conc_p_cvpq@{i | } {A : Type@{i}} {x y z : A}
   := fun p q => match q with idpath => match p with idpath => idpath end end.
 
 (** [conc (conc p q) (inv q)] です。 *)
+
 (* from: https://github.com/HoTT/HoTT/blob/756ff79da22d0804194145db775865c11c14aa48/theories/Basics/PathGroupoids.v#L124 *)
 Definition conc_cpq_vq@{i | } {A : Type@{i}} {x y z : A}
   : forall (p : Path@{i} x y) (q : Path@{i} y z),
@@ -103,6 +114,7 @@ Definition conc_cpq_vq@{i | } {A : Type@{i}} {x y z : A}
   := fun p q => match q with idpath => match p with idpath => idpath end end.
 
 (** [conc (conc p (inv q) q] です。 *)
+
 (* from: https://github.com/HoTT/HoTT/blob/756ff79da22d0804194145db775865c11c14aa48/theories/Basics/PathGroupoids.v#L131 *)
 Definition conc_cpvq_q@{i | } {A : Type@{i}} {x y z : A}
   : forall (p : Path@{i} x z) (q : Path@{i} y z),
@@ -121,6 +133,7 @@ Definition conc_cpvq_q@{i | } {A : Type@{i}} {x y z : A}
 (** ** 逆元の分配法則 *)
 
 (** [inv (conc p q)] です。 *)
+
 (* from: https://github.com/HoTT/HoTT/blob/756ff79da22d0804194145db775865c11c14aa48/theories/Basics/PathGroupoids.v#L141 *)
 Definition inv_cpq@{i | } {A : Type@{i}} {x y z : A}
   : forall (p : Path@{i} x y) (q : Path@{i} y z),
@@ -128,6 +141,7 @@ Definition inv_cpq@{i | } {A : Type@{i}} {x y z : A}
   := fun p q => match q with idpath => match p with idpath => idpath end end.
 
 (** [inv (conc (inv p) q)] です。 *)
+
 (* from: https://github.com/HoTT/HoTT/blob/756ff79da22d0804194145db775865c11c14aa48/theories/Basics/PathGroupoids.v#L148 *)
 Definition inv_cvpq@{i | } {A : Type@{i}} {x y z : A}
   : forall (p : Path@{i} x y) (q : Path@{i} x z),
@@ -135,6 +149,7 @@ Definition inv_cvpq@{i | } {A : Type@{i}} {x y z : A}
   := fun p q => match q with idpath => match p with idpath => idpath end end.
 
 (** [inv (conc p (inv q))] です。 *)
+
 (* from: https://github.com/HoTT/HoTT/blob/756ff79da22d0804194145db775865c11c14aa48/theories/Basics/PathGroupoids.v#L155 *)
 Definition inv_cpvq@{i | } {A : Type@{i}} {x y z : A}
   : forall (p : Path@{i} x z) (q : Path@{i} y z),
@@ -151,6 +166,7 @@ Definition inv_cpvq@{i | } {A : Type@{i}} {x y z : A}
     t p.
 
 (** [inv (conc (inv p) (inv q))] です。 *)
+
 (* from: https://github.com/HoTT/HoTT/blob/756ff79da22d0804194145db775865c11c14aa48/theories/Basics/PathGroupoids.v#L161 *)
 Definition inv_cvpvq@{i | } {A : Type@{i}} {x y z : A}
   : forall (p : Path@{i} y x) (q : Path@{i} z y),
@@ -160,6 +176,7 @@ Definition inv_cvpvq@{i | } {A : Type@{i}} {x y z : A}
 (** ** 逆元の逆元 *)
 
 (** [inv (inv p)] です。 *)
+
 (* from: https://github.com/HoTT/HoTT/blob/756ff79da22d0804194145db775865c11c14aa48/theories/Basics/PathGroupoids.v#L168 *)
 Definition inv_vp@{i | } {A : Type@{i}} {x y : A}
   : forall p : Path@{i} x y, Path@{i} (inv (inv p)) p
