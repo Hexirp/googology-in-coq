789d78c712bf9db83eb7b43dae48e8fcffba52d8
Hexirp <Hexirp@users.noreply.github.com>
2021-07-22 14:46:49 +0900

    Update: ブラッシュアップする

--- libraries/theories/Base.v
+++ libraries/theories/Base.v
@@ -10,6 +10,14 @@ Declare ML Module "ltac_plugin".
 Declare ML Module "ssrmatching_plugin".
 Declare ML Module "ssreflect_plugin".
 
+(** タクティックが使用できるように設定します。 *)
+
+Set Default Proof Mode "Classic".
+
+(** ビュレットを使用しないときにエラーになるように設定します。 *)
+
+Set Default Goal Selector "!".
+
 (** 帰納原理 (induction principle) を生成しないように設定します。 *)
 
 Unset Elimination Schemes.
@@ -21,10 +29,11 @@ Set Polymorphic Inductive Cumulativity.
 
 (** 関数の型を記号で書けるようにします。 *)
 
-Notation "x -> y"
-  := (forall (_ : x), y)
+Notation
+  "x -> y"
+    := (forall (_ : x), y)
   (at level 99, right associativity, y at level 200)
-  .
+.
 
 (** 関数型です。 *)
 
@@ -32,46 +41,44 @@ Module Function.
 
 (** 主型の等しさです。 *)
 
-Definition T_
-  (A : Type)
-  (A_ : A -> A -> Type)
-  (B : Type)
-  (B_ : B -> B -> Type)
+Definition T_ (A : Type) (A_ : A -> A -> Type) (B : Type) (B_ : B -> B -> Type)
   : (A -> B) -> (A -> B) -> Type
   :=
-    fun f : A -> B =>
-      fun g : A -> B =>
-        forall x : A, forall y : A, A_ x y -> B_ (f x) (g y)
-  .
+    fun (f : A -> B) (g : A -> B) =>
+      forall x : A, forall y : A, A_ x y -> B_ (f x) (g y)
+.
 
 (** 恒等関数です。 *)
 
 (* from: originally defined by Hexirp *)
-Definition id {A : Type} : A -> A
+Definition id {A : Type}
+  : A -> A
   := fun x : A => x
-  .
+.
 
 (** 定数関数です。 *)
 
 (* from: originally defined by Hexirp *)
-Definition const {A : Type} {B : Type} : A -> B -> A
+Definition const {A : Type} {B : Type}
+  : A -> B -> A
   := fun x : A => fun y : B => x
-  .
+.
 
 (** 関数の合成です。 *)
 
 (* from: originally defined by Hexirp *)
 Definition comp {A : Type} {B : Type} {C : Type}
   : (B -> C) -> (A -> B) -> A -> C
-  := fun f : B -> C => fun g : A -> B => fun x : A => f (g x)
-  .
+  := fun (f : B -> C) (g : A -> B) (x : A) => f (g x)
+.
 
 (** 関数の適用です。 *)
 
 (* from: originally defined by Hexirp *)
-Definition apply {A : Type} {B : Type} : (A -> B) -> A -> B
-  := fun f : A -> B => fun x : A => f x
-  .
+Definition apply {A : Type} {B : Type}
+  : (A -> B) -> A -> B
+  := fun (f : A -> B) (x : A) => f x
+.
 
 End Function.
 
@@ -84,8 +91,8 @@ Module Dependent_Function.
 (* from: originally defined by Hexirp *)
 Definition apply {A : Type} {B : A -> Type}
   : (forall x : A, B x) -> forall x : A, B x
-  := fun f : forall x : A, B x => fun x : A => f x
-  .
+  := fun (f : forall x : A, B x) (x : A) => f x
+.
 
 End Dependent_Function.
 
@@ -96,7 +103,10 @@ Module Unit.
 (** 主型です。 *)
 
 (* from: originally defined by Hexirp *)
-Inductive T : Type := unit : T.
+Inductive T
+  : Type
+  := unit : T
+.
 
 End Unit.
 
@@ -107,7 +117,10 @@ Module Void.
 (** 主型です。 *)
 
 (* from: originally defined by Hexirp *)
-Inductive T : Type :=.
+Inductive T
+  : Type
+  :=
+.
 
 (** 矛盾による証明です。 *)
 
@@ -115,7 +128,7 @@ Inductive T : Type :=.
 Definition absurd {A : Type}
   : T -> A
   := fun x => match x with end
-  .
+.
 
 End Void.
 
@@ -136,23 +149,23 @@ Arguments pair {A} {B} a b.
 
 (* from: originally defined by Hexirp *)
 Definition first {A : Type} {B : Type} : T A B -> A
-  := fun x => match x with pair a b => a end
-  .
+  := fun x : T A B => match x with pair a b => a end
+.
 
 (** 直積型の第二射影関数です。 *)
 
 (* from: originally defined by Hexirp *)
 Definition second {A : Type} {B : Type} : T A B -> B
-  := fun x => match x with pair a b => b end
-  .
+  := fun x : T A B => match x with pair a b => b end
+.
 
 (** 関数のカリー化です。 *)
 
 (* from: originally defined by Hexirp *)
 Definition curry {A : Type} {B : Type} {C : Type}
   : (T A B -> C) -> A -> B -> C
-  := fun f : T A B -> C => fun x : A => fun y : B => f (pair x y)
-  .
+  := fun (f : T A B -> C) (x : A) (y : B) => f (pair x y)
+.
 
 (** 関数の非カリー化です。 *)
 
@@ -160,10 +173,9 @@ Definition curry {A : Type} {B : Type} {C : Type}
 Definition uncurry {A : Type} {B : Type} {C : Type}
   : (A -> B -> C) -> T A B -> C
   :=
-    fun f : A -> B -> C =>
-      fun x : T A B =>
-        match x with pair a b => f a b end
-  .
+    fun (f : A -> B -> C) (x : T A B) =>
+      match x with pair a b => f a b end
+.
 
 End Product.
 
@@ -174,9 +186,10 @@ Module Sum.
 (** 主型です。 *)
 
 (* from: originally defined by Hexirp *)
-Inductive T (A : Type) (B : Type) : Type
+Inductive T (A : Type) (B : Type)
+  : Type
   := left : A -> T A B | right : B -> T A B
-  .
+.
 
 (** [left] についての暗黙引数を設定します。 *)
 
@@ -195,13 +208,22 @@ Module Dependent_Product.
 (** 主型です。 *)
 
 (* from: originally defined by Hexirp *)
-Inductive T (A : Type) (B : A -> Type) : Type
-  := value : (forall a : A, B a) -> T A B
-  .
+Inductive T (A : Type) (B : A -> Type)
+  : Type
+  := wrap : (forall a : A, B a) -> T A B
+.
 
 (** [value] についての暗黙引数を設定します。 *)
 
-Arguments value {A} {B} _.
+Arguments wrap {A} {B} _.
+
+(** 包みを剥がす関数です。 *)
+
+(* from: originally defined by Hexirp *)
+Definition unwrap {A : Type} {B : A -> Type}
+  : T A B -> forall a : A, B a
+  := fun x : T A B => match x with wrap x_v => x_v end
+.
 
 End Dependent_Product.
 
@@ -212,9 +234,10 @@ Module Dependent_Sum.
 (** 主型です。 *)
 
 (* from: originally defined by Hexirp *)
-Inductive T (A : Type) (B : A -> Type) : Type
+Inductive T (A : Type) (B : A -> Type)
+  : Type
   := pair : forall a : A, B a -> T A B
-  .
+.
 
 (** [pair] についての暗黙引数を設定します。 *)
 
@@ -223,16 +246,18 @@ Arguments pair {A} {B} a b.
 (** 依存和型の第一射影関数です。 *)
 
 (* from: originally defined by Hexirp *)
-Definition first {A : Type} {B : A -> Type} : T A B -> A
+Definition first {A : Type} {B : A -> Type}
+  : T A B -> A
   := fun x => match x with pair a b => a end
-  .
+.
 
 (** 依存和型の第二射影関数です。 *)
 
 (* from: originally defined by Hexirp *)
-Definition second {A : Type} {B : A -> Type} : forall x : T A B, B (first x)
+Definition second {A : Type} {B : A -> Type}
+  : forall x : T A B, B (first x)
   := fun x => match x with pair a b => b end
-  .
+.
 
 End Dependent_Sum.
 
@@ -243,9 +268,10 @@ Module Path.
 (** 主型です。 *)
 
 (* from: originally defined by Hexirp *)
-Inductive T (A : Type) (a : A) : A -> Type
+Private Inductive T (A : Type) (a : A) 
+  : A -> Type
   := id : T A a a
-  .
+.
 
 (** 道型についての暗黙引数を設定します。 *)
 
@@ -261,43 +287,44 @@ Arguments id {A} {a}, [A] a.
 (** 道の逆です。 *)
 
 (* from: originally defined by Hexirp *)
-Definition inv {A : Type} {x y : A} : T x y -> T y x
+Definition inv {A : Type} {x y : A}
+  : T x y -> T y x
   := fun p : T x y => match p with id => id end
-  .
+.
 
 (** 道の結合です。 *)
 
 (* from: originally defined by Hexirp *)
-Definition conc {A : Type} {x y z : A} : T x y -> T y z -> T x z
+Definition conc {A : Type} {x y z : A}
+  : T x y -> T y z -> T x z
   :=
-    fun p : T x y =>
-      fun q : T y z =>
-        match q with id => match p with id => id end end
-  .
+    fun (p : T x y) (q : T y z) =>
+      match q with id => match p with id => id end end
+.
 
 (** 道の結合と逆です。 *)
 
 (* from: originally defined by Hexirp *)
 Definition conv {A : Type} {x y z : A}
   : T x y -> T x z -> T y z
-  := fun p : T x y => fun q : T x z => conc (inv p) q
-  .
+  := fun (p : T x y) (q : T x z) => conc (inv p) q
+.
 
 (** 道による輸送です。 *)
 
 (* from: originally defined by Hexirp *)
 Definition trpt {A : Type} {B : A -> Type} {x y : A}
   : T x y -> B x -> B y
-  := fun p : T x y => fun u : B x => match p with id => u end
-  .
+  := fun (p : T x y) (u : B x) => match p with id => u end
+.
 
 (** 道による輸送と逆です。 *)
 
 (* from: originally defined by Hexirp *)
 Definition trpv {A : Type} {B : A -> Type} {x y : A}
   : T x y -> B y -> B x
-  := fun p : T x y => fun u : B y => trpt (inv p) u
-  .
+  := fun (p : T x y) (u : B y) => trpt (inv p) u
+.
 
 (** 道への適用です。 *)
 
@@ -305,7 +332,7 @@ Definition trpv {A : Type} {B : A -> Type} {x y : A}
 Definition ap {A : Type} {B : Type} (f : A -> B) {x y : A}
   : T x y -> T (f x) (f y)
   := fun p : T x y => match p with id => id end
-  .
+.
 
 End Path.
 
@@ -316,30 +343,30 @@ Module Pointwise_Path.
 (** 主型です。 *)
 
 (* from: originally defined by Hexirp *)
-Definition T (A : Type) (B : Type) : (A -> B) -> (A -> B) -> Type
+Definition T (A : Type) (B : Type)
+  : (A -> B) -> (A -> B) -> Type
   :=
-    fun f : A -> B =>
-      fun g : A -> B =>
-        forall x : A, Path.T (f x) (g x)
-  .
+    fun (f : A -> B) (g : A -> B) =>
+      forall x : A, Path.T (f x) (g x)
+.
 
 (** [Function.T_] への変換です。 *)
 
 (* from: originally defined by Hexirp *)
 Definition to_Function_1 (A : Type) (B : Type)
   :
-    forall f : A -> B,
-      forall g : A -> B,
-        T A B f g -> Function.T_ A (@Path.T A) B (@Path.T B) f g
-  .
+    forall (f : A -> B) (g : A -> B),
+      T A B f g -> Function.T_ A (@Path.T A) B (@Path.T B) f g
+.
 Proof.
-  refine (fun f : A -> B => fun g : A -> B => _).
-  unfold T.
-  unfold Function.T_.
-  refine (fun h : forall x : A, Path.T (f x) (g x) => _).
-  refine (fun x : A => fun y : A => _).
-  refine (fun p : Path.T x y => _).
-  refine (match p with Path.id => _ end).
+  move=> f g.
+  unfold T; unfold Function.T_.
+  move=> h.
+  move=> x y.
+  move=> p.
+  refine (let D := fun y : A => Path.T (f x) (g y) in _).
+  change (D y).
+  refine (Path.trpt p _).
   exact (h x).
 Defined.
 
@@ -348,24 +375,14 @@ Defined.
 (* from: originally defined by Hexirp *)
 Definition from_Function_1 (A : Type) (B : Type)
   :
-    forall f : A -> B,
-      forall g : A -> B,
-        Function.T_ A (@Path.T A) B (@Path.T B) f g -> T A B f g
-  .
+    forall (f : A -> B) (g : A -> B),
+      Function.T_ A (@Path.T A) B (@Path.T B) f g -> T A B f g
+.
 Proof.
-  refine (fun f : A -> B => fun g : A -> B => _).
-  unfold Function.T_.
-  unfold T.
-  refine
-    (
-      fun
-        h
-          :
-            forall x : A, forall y : A, Path.T x y -> Path.T (f x) (g y)
-      => _
-    )
-    .
-  refine (fun x : A => _).
+  move=> f g.
+  unfold Function.T_; unfold T.
+  move=> h.
+  move=> x.
   exact (h x x Path.id).
 Defined.
 
