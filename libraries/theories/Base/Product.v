c4cc164cb3d38a90dacbc8775865826002098e17
Hexirp <Hexirp@users.noreply.github.com>
2021-10-23 23:40:15 +0900

    Update: Base.Product に帰納法の原理を導入する

--- libraries/theories/Base/Product.v
+++ libraries/theories/Base/Product.v
@@ -20,15 +20,39 @@ Arguments pair {A} {B} a b.
 
 (** [pair] についての暗黙引数を設定します。 *)
 
+Definition induction
+    {A : Type}
+    {B : Type}
+    (P : T A B -> Type)
+    (construct_pair : forall (x_1 : A) (x_2 : B), P (pair x_1 x_2))
+  : forall x : T A B, P x
+  := fun x : T A B => match x with pair a b => construct_pair a b end
+.
+(* from: originally defined by Hexirp *)
+
+(** 帰納法です。 *)
+
+Definition recursion
+    {A : Type}
+    {B : Type}
+    {P : Type}
+    (construct_pair : A -> B -> P)
+  : T A B -> P
+  := induction (fun x_ : T A B => P) construct_pair
+.
+(* from: originally defined by Hexirp *)
+
+(** 再帰です。 *)
+
 Definition first {A : Type} {B : Type} : T A B -> A
-  := fun x : T A B => match x with pair a b => a end
+  := recursion (fun (a : A) (b : B) => a)
 .
 (* from: originally defined by Hexirp *)
 
 (** 直積型の第一射影関数です。 *)
 
 Definition second {A : Type} {B : Type} : T A B -> B
-  := fun x : T A B => match x with pair a b => b end
+  := recursion (fun (a : A) (b : B) => b)
 .
 (* from: originally defined by Hexirp *)
 
@@ -37,8 +61,8 @@ Definition second {A : Type} {B : Type} : T A B -> B
 Definition map {A : Type} {B : Type} {C : Type} {D : Type}
   : (A -> C) -> (B -> D) -> T A B -> T C D
   :=
-    fun (f_a : A -> C) (f_b : B -> D) (x : T A B) =>
-      match x with pair a b => pair (f_a a) (f_b b) end
+    fun (f_a : A -> C) (f_b : B -> D) =>
+      recursion (fun (a : A) (b : B) => pair (f_a a) (f_b b))
 .
 (* from: originally defined by Hexirp *)
 
@@ -55,8 +79,8 @@ Definition curry {A : Type} {B : Type} {C : Type}
 Definition uncurry {A : Type} {B : Type} {C : Type}
   : (A -> B -> C) -> T A B -> C
   :=
-    fun (f : A -> B -> C) (x : T A B) =>
-      match x with pair a b => f a b end
+    fun f : A -> B -> C =>
+      recursion (fun (a : A) (b : B) => f a b)
 .
 (* from: originally defined by Hexirp *)
 
