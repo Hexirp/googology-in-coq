3d9d1226b0b70599d62f3a7bf576d24f64554729
Hexirp <Hexirp@users.noreply.github.com>
2021-10-23 23:52:53 +0900

    Update: Base.Sum に帰納法の原理を導入する

--- libraries/theories/Base/Sum.v
+++ libraries/theories/Base/Sum.v
@@ -23,3 +23,35 @@ Arguments left {A} {B} a.
 Arguments right {A} {B} b.
 
 (** [right] についての暗黙引数を設定します。 *)
+
+Definition induction
+    {A : Type}
+    {B : Type}
+    (P : T A B -> Type)
+    (construct_left : forall x_L : A, P (left x_L))
+    (construct_right : forall x_R : B, P (right x_R))
+  : forall x : T A B, P x
+  :=
+    fun x : T A B =>
+      match x with
+          left x_L => construct_left x_L
+        |
+          right x_R => construct_right x_R
+      end
+.
+(* from: originally defined by Hexirp *)
+
+(** 帰納法です。 *)
+
+Definition recursion
+    {A : Type}
+    {B : Type}
+    {P : Type}
+    (construct_left : A -> P)
+    (construct_right : B -> P)
+  : T A B -> P
+  := induction (fun x_ : T A B => P) construct_left construct_right
+.
+(* from: originally defined by Hexirp *)
+
+(** 再帰です。 *)
