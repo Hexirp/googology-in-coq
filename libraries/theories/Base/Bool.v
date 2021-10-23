307ac018af14eceab14fa37a5cea9eaec97e9bf7
Hexirp <Hexirp@users.noreply.github.com>
2021-10-24 01:56:42 +0900

    Update: Base.Bool を更新する
    不平等の定理を、いちいち全ての構築子について導出しなくても済むように。
    
    たとえば、 `define inductive type Color : Type 0 { red : T; yellow : T;
    blue : T; }` という型を定義したら `red` と `yellow` は等しくないとか、
    そういう定理が必要になってくる訳である。これを自動的に導出する戦術が
    inversion なのであるが、こういうのは構成主義として好ましくない。
    なので、 `Sum Unit (Sum Unit Unit)` というようにすると、不平等の定理が
    `Sum` にたいするものだけで済むのである。

--- libraries/theories/Base/Bool.v
+++ libraries/theories/Base/Bool.v
@@ -1,6 +1,8 @@
 (** ブール型に関するモジュールです。 *)
 
 Require Googology_In_Coq.Base.Base.
+Require Googology_In_Coq.Base.Unit.
+Require Googology_In_Coq.Base.Sum.
 
 (** [Googology_In_Coq.Base.Base] を要求します。 *)
 
@@ -8,13 +10,20 @@ Import Googology_In_Coq.Base.Base.
 
 (** [Googology_In_Coq.Base.Base] を開きます。 *)
 
-Inductive T
-  : Type
-  := true : T | false : T
-.
-(* from: originally defined by Hexirp *)
+Definition T : Type := Sum.T Unit.T Unit.T.
+(* from: https://www.cs.bham.ac.uk/~mhe/HoTT-UF-in-Agda-Lecture-Notes/HoTT-UF-Agda.html *)
 
 (** 主型です。 *)
+
+Definition true : T := Sum.left Unit.unit.
+(* from: originally defined by Hexirp *)
+
+(** 一つ目の構築子です。 *)
+
+Definition false : T := Sum.right Unit.unit.
+(* from: originally defined by Hexirp *)
+
+(** 二つ目の構築子です。 *)
 
 Definition induction
     (P : T -> Type)
@@ -22,12 +31,10 @@ Definition induction
     (construct_false : P false)
   : forall x : T, P x
   :=
-    fun x : T =>
-      match x with
-          true => construct_true
-        |
-          false => construct_false
-      end
+    Sum.induction
+      P
+      (Unit.induction (fun x_ : Unit.T => P (Sum.left x_)) construct_true)
+      (Unit.induction (fun x_ : Unit.T => P (Sum.right x_)) construct_false)
 .
 (* from: originally defined by Hexirp *)
 
