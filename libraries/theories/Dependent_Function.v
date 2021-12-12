6a37b08f40a50b09f2a5c5c93a592eb7c101f237
Hexirp <Hexirp@users.noreply.github.com>
2021-12-13 02:52:41 +0900

    Update: Dependent_Function モジュールを
    可述的になるようにする。

--- libraries/theories/Dependent_Function.v
+++ libraries/theories/Dependent_Function.v
@@ -8,31 +8,32 @@ Import Googology_In_Coq.Base.
 
 (** [Googology_In_Coq.Base] を開きます。 *)
 
-Axiom Dependent_Function@{i j | }
-  :
-    forall A : Type@{i}, (A -> Type@{j}) -> Type@{j}
+Definition Dependent_Function@{i | }
+    (A : Type@{i})
+    (B : A -> Type@{i})
+  : Type@{i}
+  := forall x : A, B x
 .
 (* from: originally defined by Hexirp *)
 
 (** 依存関数型です。 *)
 
-Axiom abstract@{i j | }
-  :
-    forall
-      (A : Type@{i})
-      (B : A -> Type@{j})
-    ,
-      (forall x : A, B x) -> Dependent_Function@{i j} A B
+Definition abstract@{i | }
+    (A : Type@{i})
+    (B : A -> Type@{j})
+    (x : forall x : A, B x)
+  : Dependent_Function@{i j} A B
+  := x
 .
 
 (** 抽象です。ラムダ抽象です。 *)
 
-Axiom apply@{i j | }
-  :
-    forall                                                                            (A : Type@{i})
-      (B : A -> Type@{j})
-    ,
-      Dependent_Function@{i j} A B -> forall x : A, B x
+Definition apply@{i j | }
+    (A : Type@{i})
+    (B : A -> Type@{j})
+    (x : Dependent_Function@{i j} A B)
+  : forall x : A, B x
+  := x
 .
 
 (** 適用です。 *)
