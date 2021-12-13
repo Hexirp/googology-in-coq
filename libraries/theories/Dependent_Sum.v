901f93657aa66af52a4fa6afbdc4d14da044fde5
Hexirp <Hexirp@users.noreply.github.com>
2021-12-13 18:24:59 +0900

    Update: Dependent_Sum を書き直す

--- libraries/theories/Dependent_Sum.v
+++ libraries/theories/Dependent_Sum.v
@@ -1,71 +1,62 @@
-(** 依存和の型に関するモジュールです。 *)
+(** 依存直和型に関するモジュールです。 *)
 
-Require Googology_In_Coq.Base.Base.
+Require Googology_In_Coq.Base.
 
-(** [Googology_In_Coq.Base.Base] を要求します。 *)
+(** ライブラリを要求します。 *)
 
-Import Googology_In_Coq.Base.Base.
+Import Googology_In_Coq.Base.
 
-(** [Googology_In_Coq.Base.Base] を開きます。 *)
+(** ライブラリを開きます。 *)
 
-Inductive T (A : Type) (B : A -> Type)
-  : Type
-  := pair : forall a : A, B a -> T A B
+Inductive Dependent_Sum@{i | } (A : Type@{i}) (B : A -> Type@{i}) : Type@{i}
+  := pair : forall a : A, B a -> Dependent_Sum A B
 .
 (* from: originally defined by Hexirp *)
 
-(** 主型です。 *)
+(** 依存直和型です。 *)
 
 Arguments pair {A} {B} a b.
 
 (** [pair] についての暗黙引数を設定します。 *)
 
-Definition induction
-    {A : Type}
-    {B : A -> Type}
-    (P : T A B -> Type)
+Definition matching@{i | }
+    {A : Type@{i}}
+    {B : A -> Type@{i}}
+    (P : Dependent_Sum A B -> Type@{i})
     (construct_pair : forall (a : A) (b : B a), P (pair a b))
-  : forall x : T A B, P x
-  := fun x : T A B => match x with pair a b => construct_pair a b end
-.
-(* from: originally defined by Hexirp *)
-
-(** 帰納法です。 *)
-
-Definition recursion
-    {A : Type}
-    {B : A -> Type}
-    {P : Type}
-    (construct_pair : forall a : A, B a -> P)
-  : T A B -> P
-  := induction (fun x_ : T A B => P) construct_pair
-.
-(* from: originally defined by Hexirp *)
-
-(** 再帰です。 *)
-
-Definition first {A : Type} {B : A -> Type}
-  : T A B -> A
-  := recursion (fun (a : A) (b : B a) => a)
-.
-(* from: originally defined by Hexirp *)
-
-(** 依存和型の第一射影関数です。 *)
-
-Definition second {A : Type} {B : A -> Type}
-  : forall x : T A B, B (first x)
-  := induction (fun x_ : T A B => B (first x_)) (fun (a : A) (b : B a) => b)
-.
-(* from: originally defined by Hexirp *)
-
-(** 依存和型の第二射影関数です。 *)
-
-Definition map {A : Type} {B : A -> Type} {C : A -> Type}
-  : (forall x : A, B x -> C x) -> T A B -> T A C
+  : forall x : Dependent_Sum A B, P x
   :=
-    fun f : forall x : A, B x -> C x =>
-      recursion (fun (a : A) (b : B a) => pair a (f a b))
+    fun x : Dependent_Sum A B =>
+      match x with pair a b => construct_pair a b end
 .
 (* from: originally defined by Hexirp *)
 
-(** 依存和型の写像です。 *)
+(** 場合分けです。 *)
+
+Definition matching_nodep@{i | }
+    {A : Type@{i}}
+    {B : A -> Type@{i}}
+    {P : Type@{i}}
+    (construct_pair : forall a : A, B a -> P)
+  : Dependent_Sum A B -> P
+  := matching (fun x_ : Dependent_Sum A B => P) construct_pair
+.
+(* from: originally defined by Hexirp *)
+
+(** 場合分けです。 *)
+
+Definition first@{i | } {A : Type@{i}} {B : A -> Type@{i}}
+  : Dependent_Sum A B -> A
+  := matching_nodep (fun (a : A) (b : B a) => a)
+.
+(* from: originally defined by Hexirp *)
+
+(** 依存直和型の第一射影関数です。 *)
+
+Definition second@{i | } {A : Type@{i}} {B : A -> Type@{i}}
+  : forall x : Dependent_Sum A B, B (first x)
+  := matching (fun x_ : T A B => B (first x_)) (fun (a : A) (b : B a) => b)
+.
+(* from: originally defined by Hexirp *)
+
+(** 依存直和型の第二射影関数です。 *)
