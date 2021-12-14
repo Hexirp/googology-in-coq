020027e0fe42af8e95da065d490ce93c2ac49a1b
Hexirp <Hexirp@users.noreply.github.com>
2021-12-14 19:45:49 +0900

    Update: Universe を書き直す

--- libraries/theories/Universe.v
+++ libraries/theories/Universe.v
@@ -1,20 +1,20 @@
-(** 型の型です。 *)
+(** 超型です。 *)
 
-Require Googology_In_Coq.Base.Base.
+Require Googology_In_Coq.Base.
 
 (** ライブラリを要求します。 *)
 
-Import Googology_In_Coq.Base.Base.
+Import Googology_In_Coq.Base.
 
-(** [Googology_In_Coq.Base.Base] を開きます。 *)
+(** ライブラリを開きます。 *)
 
-Definition T : Type := Type.
+Definition Universe@{i s_i | i < s_i} : Type@{s_i} := Type@{i}.
 (* from: originally defined by Hexirp *)
 
-(** 主型です。 *)
+(** 超型です。 *)
 
-Definition of {A : Type}
-  : A -> Type
+Definition of@{i s_i | i < s_i} {A : Type@{i}}
+  : A -> Type@{s_i}
   := fun x : A => A
 .
 (* from: https://www.cs.bham.ac.uk/~mhe/HoTT-UF-in-Agda-Lecture-Notes/ *)
