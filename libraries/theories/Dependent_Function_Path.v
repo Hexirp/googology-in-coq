ff63727bcd0428f99117e1e9adba6f4a6c5c5532
Hexirp <Hexirp@users.noreply.github.com>
2021-12-15 17:01:16 +0900

    Add: Dependent_Function_Path を

---
+++ libraries/theories/Dependent_Function_Path.v
@@ -0,0 +1,21 @@
+(** 依存関数型の道のモジュールです。 *)
+
+Require Googology_In_Coq.Base.
+Require Googology_In_Coq.Dependent_Function.
+Require Googology_In_Coq.Path.
+
+(** ライブラリを要求します。 *)
+
+Import Googology_In_Coq.Base.
+Import Googology_In_Coq.Dependent_Function (Dependent_Function).
+Import Googology_In_Coq.Path (Path).
+
+(** ライブラリを開きます。 *)
+
+Definition
+  Dependent_Function_Path@{i | } (A : Type@{i}) (B : A -> Type@{i})
+    : Dependent_Function A B -> Dependent_Function A B -> Type@{i}
+    := Path_Visible (Dependent_Function A B)
+.
+
+(** 依存関数型の道です。 *)
