deb384b6c9b46c9219f9b2c3975aa9f7cb7e3a54
Hexirp <Hexirp@users.noreply.github.com>
2022-01-04 18:31:12 +0900

    Add: Peano_Number を

---
+++ libraries/theories/Peano_Number.v
@@ -0,0 +1,30 @@
+(** 自然数の型に関するモジュールです。 *)
+
+Require Googology_In_Coq.Base.
+Require Googology_In_Coq.Function.
+Require Googology_In_Coq.Sum.
+Require Googology_In_Coq.Void.
+Require Googology_In_Coq.Unit.
+Require Googology_In_Coq.W_type.
+
+(** ライブラリを要求します。 *)
+
+Import Googology_In_Coq.Base.
+Import Googology_In_Coq.Function (Function).
+Import Googology_In_Coq.Sum (Sum).
+Import Googology_In_Coq.Void (Void).
+Import Googology_In_Coq.Unit (Unit).
+Import Googology_In_Coq.W_type (W_type)
+
+(** ライブラリを開きます。 *)
+
+Definition
+  Peano_Number@{i s_i | i < s_i} : Type@{i}
+    :=
+      W_type@{i}
+        Bool@{i}
+        (matching_nodep_visible@{s_i} Type@{i} Void@{i} Unit@{i})
+.
+(* from: originally defined by Hexirp *)
+
+(** 自然数の型です。 *)
