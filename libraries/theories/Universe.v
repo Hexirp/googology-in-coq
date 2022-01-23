d72861720fe159c76be78d43ba67e78c727dd435
Hexirp <Hexirp@users.noreply.github.com>
2022-01-23 18:53:03 +0900

    Update: Universe を
    暗黙引数を使わない。

--- libraries/theories/Universe.v
+++ libraries/theories/Universe.v
@@ -12,11 +12,3 @@ Definition Universe@{i s_i | i < s_i} : Type@{s_i} := Type@{i}.
 (* from: originally defined by Hexirp *)
 
 (** 超型です。 *)
-
-Definition of@{i s_i | i < s_i} {A : Type@{i}}
-  : A -> Type@{s_i}
-  := fun x : A => A
-.
-(* from: https://www.cs.bham.ac.uk/~mhe/HoTT-UF-in-Agda-Lecture-Notes/ *)
-
-(** 値の型です。 *)
