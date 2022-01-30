85cd4ba8066fdf56bd1dc058778027143cdc1916
Hexirp <Hexirp@users.noreply.github.com>
2022-01-31 02:19:59 +0900

    Update: Peano_Number.Beta を
    Peano_Number.Beta.Zero と Peano_Number.Beta.Succ を使う。

--- libraries/theories/Peano_Number/Beta.v
+++ libraries/theories/Peano_Number/Beta.v
@@ -1,24 +1,32 @@
 (** 自然数の型のベータに関するモジュールです。 *)
 
 Require Googology_In_Coq.Base.
-Require Googology_In_Coq.Void.
-Require Googology_In_Coq.Unit.
 Require Googology_In_Coq.Universe.
 Require Googology_In_Coq.Peano_Number.Alpha.
+Require Googology_In_Coq.Peano_Number.Beta.Zero.
+Require Googology_In_Coq.Peano_Number.Beta.Succ.
 
 (** ライブラリを要求します。 *)
 
 Import Googology_In_Coq.Base.
-Import Googology_In_Coq.Void (Void).
-Import Googology_In_Coq.Unit (Unit).
 Import Googology_In_Coq.Universe (Universe).
 Import Googology_In_Coq.Peano_Number.Alpha (Alpha).
 
 (** ライブラリを開きます。 *)
 
+Definition Zero@{i | } : Type@{i} := Zero.Zero@{i}.
+(* from: originally defined by Hexirp *)
+
+(** 自然数の型のベータの [zero] です。 *)
+
+Definition Succ@{i | } : Type@{i} := Succ.Succ@{i}.
+(* from: originally defined by Hexirp *)
+
+(** 自然数の型のベータの [succ] です。 *)
+
 Definition
   Beta@{i s_i | i < s_i} : Alpha@{i} -> Type@{i}
-    := Alpha.matching_nodep Universe@{i s_i} Void@{i} Unit@{i}
+    := Alpha.matching_nodep Universe@{i s_i} Zero@{i} Succ@{i}
 .
 (* from: originally defined by Hexirp *)
 
@@ -26,7 +34,7 @@ Definition
 
 Definition
   zero@{i s_i | i < s_i} (A : Type@{i}) : Beta Alpha.zero -> A
-    := Void.matching_nodep A
+    := Zero.matching_nodep A
 .
 (* from: originally defined by Hexirp *)
 
@@ -34,7 +42,7 @@ Definition
 
 Definition
   succ@{i s_i | i < s_i} (A : Type@{i}) (x_p : A) : Beta Alpha.succ -> A
-    := Unit.matching_nodep A x_p
+    := Succ.matching_nodep A x_p
 .
 (* from: originally defined by Hexirp *)
 
