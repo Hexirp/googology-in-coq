c184cd1eeaebd8fd28c47bef6e65ce9f9ef07474
Hexirp <Hexirp@users.noreply.github.com>
2022-03-03 18:46:22 +0900

    Update: Peano_Number_Alpha.Alpha の名前を Peano_Number_Alpha にする

--- libraries/theories/Peano_Number_Alpha.v
+++ libraries/theories/Peano_Number_Alpha.v
@@ -12,27 +12,27 @@ Import Googology_In_Coq.Unit (Unit).
 
 (** ライブラリを開きます。 *)
 
-Definition Alpha@{i | } : Type@{i} := Sum@{i} Unit@{i} Unit@{i}.
+Definition Peano_Number_Alpha@{i | } : Type@{i} := Sum@{i} Unit@{i} Unit@{i}.
 (* from: originally defined by Hexirp *)
 
 (** 自然数の型のアルファです。 *)
 
-Definition zero@{i | } : Alpha@{i} := Sum.left Unit@{i} Unit@{i} Unit.unit.
+Definition zero@{i | } : Peano_Number_Alpha@{i} := Sum.left Unit@{i} Unit@{i} Unit.unit.
 (* from: originally defined by Hexirp *)
 
 (** 自然数の型のアルファの第一構築子です。 *)
 
-Definition succ@{i | } : Alpha@{i} := Sum.right Unit@{i} Unit@{i} Unit.unit.
+Definition succ@{i | } : Peano_Number_Alpha@{i} := Sum.right Unit@{i} Unit@{i} Unit.unit.
 (* from: originally defined by Hexirp *)
 
 (** 自然数の型のアルファの第二構築子です。 *)
 
 Definition
   matching@{i | }
-      (P : Alpha@{i} -> Type@{i})
+      (P : Peano_Number_Alpha@{i} -> Type@{i})
       (constructor_zero : P zero)
       (constructor_succ : P succ)
-    : forall x : Alpha@{i}, P x
+    : forall x : Peano_Number_Alpha@{i}, P x
     :=
       Sum.matching
         Unit@{i}
@@ -58,8 +58,8 @@ Definition
       (P : Type@{i})
       (constructor_zero : P)
       (constructor_succ : P)
-  : Alpha@{i} -> P
-  := matching (fun x_ : Alpha@{i} => P) constructor_zero constructor_succ
+  : Peano_Number_Alpha@{i} -> P
+  := matching (fun x_ : Peano_Number_Alpha@{i} => P) constructor_zero constructor_succ
 .
 (* from: originally defined by Hexirp *)
 
--- libraries/theories/Peano_Number_Beta.v
+++ libraries/theories/Peano_Number_Beta.v
@@ -10,22 +10,22 @@ Require Googology_In_Coq.Peano_Number_Beta_Succ.
 
 Import Googology_In_Coq.Base.
 Import Googology_In_Coq.Universe (Universe).
-Import Googology_In_Coq.Peano_Number_Alpha (Alpha).
+Import Googology_In_Coq.Peano_Number_Alpha (Peano_Number_Alpha).
 Import Googology_In_Coq.Peano_Number_Beta_Zero (Zero).
 Import Googology_In_Coq.Peano_Number_Beta_Succ (Succ).
 
 (** ライブラリを開きます。 *)
 
 Definition
-  Beta@{i s_i | i < s_i} : Alpha@{i} -> Type@{i}
-    := Alpha.matching_nodep Universe@{i s_i} Zero@{i} Succ@{i}
+  Beta@{i s_i | i < s_i} : Peano_Number_Alpha@{i} -> Type@{i}
+    := Peano_Number_Alpha.matching_nodep Universe@{i s_i} Zero@{i} Succ@{i}
 .
 (* from: originally defined by Hexirp *)
 
 (** 自然数の型のベータです。 *)
 
 Definition
-  zero@{i s_i | i < s_i} (A : Type@{i}) : Beta Alpha.zero -> A
+  zero@{i s_i | i < s_i} (A : Type@{i}) : Beta Peano_Number_Alpha.zero -> A
     := Zero.matching_nodep A
 .
 (* from: originally defined by Hexirp *)
@@ -33,7 +33,7 @@ Definition
 (** 自然数の型のベータの [zero] です。 *)
 
 Definition
-  succ@{i s_i | i < s_i} (A : Type@{i}) (x_p : A) : Beta Alpha.succ -> A
+  succ@{i s_i | i < s_i} (A : Type@{i}) (x_p : A) : Beta Peano_Number_Alpha.succ -> A
     := Succ.matching_nodep A x_p
 .
 (* from: originally defined by Hexirp *)
