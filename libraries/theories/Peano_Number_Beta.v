dbc6e21e709b7ec81a38a315218806f1bd11f5ff
Hexirp <Hexirp@users.noreply.github.com>
2022-03-04 00:51:05 +0900

    Update: Peano_Number_Beta の Beta を Peano_Number_Beta に

--- libraries/theories/Peano_Number_Beta.v
+++ libraries/theories/Peano_Number_Beta.v
@@ -17,7 +17,7 @@ Import Googology_In_Coq.Peano_Number_Beta_Succ (Peano_Number_Beta_Succ).
 (** ライブラリを開きます。 *)
 
 Definition
-  Beta@{i s_i | i < s_i} : Peano_Number_Alpha@{i} -> Type@{i}
+  Peano_Number_Beta@{i s_i | i < s_i} : Peano_Number_Alpha@{i} -> Type@{i}
     := Peano_Number_Alpha.matching_nodep Universe@{i s_i} Peano_Number_Beta_Zero@{i} Peano_Number_Beta_Succ@{i}
 .
 (* from: originally defined by Hexirp *)
@@ -25,7 +25,7 @@ Definition
 (** 自然数の型のベータです。 *)
 
 Definition
-  zero@{i s_i | i < s_i} (A : Type@{i}) : Beta Peano_Number_Alpha.zero -> A
+  zero@{i s_i | i < s_i} (A : Type@{i}) : Peano_Number_Beta Peano_Number_Alpha.zero -> A
     := Peano_Number_Beta_Zero.matching_nodep A
 .
 (* from: originally defined by Hexirp *)
@@ -33,7 +33,7 @@ Definition
 (** 自然数の型のベータの [zero] です。 *)
 
 Definition
-  succ@{i s_i | i < s_i} (A : Type@{i}) (x_p : A) : Beta Peano_Number_Alpha.succ -> A
+  succ@{i s_i | i < s_i} (A : Type@{i}) (x_p : A) : Peano_Number_Beta Peano_Number_Alpha.succ -> A
     := Peano_Number_Beta_Succ.matching_nodep A x_p
 .
 (* from: originally defined by Hexirp *)
