8aa3d77ab3b4354607caa8da7c1ad3b4ea629048
Hexirp <Hexirp@users.noreply.github.com>
2022-03-04 18:16:48 +0900

    Add: Naive_Functional_Extensionality を
    Peano_Number で必要になるため。

---
+++ libraries/theories/Naive_Functional_Extensionality.v
@@ -0,0 +1,45 @@
+(** 単純な関数外延性のモジュールです。 *)
+
+Require Googology_In_Coq.Base.
+Require Googology_In_Coq.Dependent_Function.
+Require Googology_In_Coq.Function.
+Require Googology_In_Coq.Path.
+Require Googology_In_Coq.Pointwise_Path.
+
+(** ライブラリを要求します。 *)
+
+Import Googology_In_Coq.Base.
+Import Googology_In_Coq.Dependent_Function (Dependent_Function).
+Import Googology_In_Coq.Function (Function).
+Import Googology_In_Coq.Path (Path).
+Import Googology_In_Coq.Pointwise_Path (Pointwise_Path).
+
+(** ライブラリを開きます。 *)
+
+Definition
+  Naive_Functional_Extensionality@{i s_i | i < s_i}
+      (A : Type@{i})
+      (B : Type@{i})
+    : Type@{s_i}
+    :=
+      Dependent_Function@{s_i}
+        (Function@{i} A B)
+        Universe@{i s_i}
+        (
+          fun f : Function@{i} A B =>
+            Dependent_Function@{s_i}
+              (Function@{i} A B)
+              Universe@{i s_i}
+              (
+                fun g : Function@{i} A B =>
+                  Function@{s_i}
+                    Universe@{i s_i}
+                    Universe@{i s_i}
+                    (Pointwise_Path@{i} A B f g)
+                    (Path@{i} (Function@{i} A B) f g)
+              )
+        )
+.
+(* from: originally defined by Hexirp *)
+
+(** 単純な関数外延性です。 *)
