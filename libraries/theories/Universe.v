8d6c91824b2bfe0c847d43da9534f98253e258bb
Hexirp <Hexirp@users.noreply.github.com>
2022-03-06 18:15:46 +0900

    Update: Universe を
    newtype 方式を採用する。

--- libraries/theories/Universe.v
+++ libraries/theories/Universe.v
@@ -8,7 +8,17 @@ Import Googology_In_Coq.Base.
 
 (** ライブラリを開きます。 *)
 
-Definition Universe@{i s_i | i < s_i} : Type@{s_i} := Type@{i}.
+Inductive
+  Universe@{i s_i | i < s_i} : Type@{s_i} := wrap : Type@{i} -> Universe
+.
+(* from: originally defined by Hexirp *)
+
+(** 超型です。 *)
+
+Definition
+  unwrap@{i s_i | i < s_i} : Universe@{i s_i} -> Type@{i}
+    := fun x : Universe@{i s_i} => match x with wrap x_v => x_v end
+.
 (* from: originally defined by Hexirp *)
 
 (** 超型です。 *)
