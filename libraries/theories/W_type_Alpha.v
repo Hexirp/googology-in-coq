f053a1bd89aff90002063076f6419f802d49f238
Hexirp <Hexirp@users.noreply.github.com>
2022-03-06 18:06:43 +0900

    Update: W_type_Alpha を
    newtype 方式を採用する。

--- libraries/theories/W_type_Alpha.v
+++ libraries/theories/W_type_Alpha.v
@@ -12,17 +12,30 @@ Import Googology_In_Coq.W_type_Beta (W_type_Beta).
 
 (** ライブラリを開きます。 *)
 
-Definition
+Inductive
   W_type_Alpha@{i | }
       (t : forall (A : Type@{i}) (B : A -> Type@{i}), Type@{i})
       (A : Type@{i})
       (B : A -> Type@{i})
     : Type@{i}
-    := Dependent_Sum A (W_type_Beta t A B)
+    :=
+      wrap
+        : Dependent_Sum@{i} A (W_type_Beta@{i} t A B) -> W_type_Alpha t A B
 .
 (* from: originally defined by Hexirp *)
 
 (** ウ型のアルファです。 *)
+
+Definition
+  unwrap@{i | }
+      (t : forall (A : Type@{i}) (B : A -> Type@{i}), Type@{i})
+      (A : Type@{i})
+      (B : A -> Type@{i})
+    : W_type_Alpha@{i} t A B -> Dependent_Sum@{i} A (W_type_Beta@{i} t A B)
+    :=
+      fun x : W_type_Alpha@{i} t A B =>
+        match x with wrap _ _ _ x_v => x_v end
+.
 
 Definition
   pair@{i | }
@@ -30,9 +43,9 @@ Definition
       (A : Type@{i})
       (B : A -> Type@{i})
       (a : A)
-      (b : W_type_Beta t A B a)
-    : W_type_Alpha t A B
-    := Dependent_Sum.pair A (W_type_Beta t A B) a b
+      (b : W_type_Beta@{i} t A B a)
+    : W_type_Alpha@{i} t A B
+    := wrap t A B (Dependent_Sum.pair A (W_type_Beta@{i} t A B) a b)
 .
 (* from: originally defined by Hexirp *)
 
@@ -43,13 +56,27 @@ Definition
       (t : forall (A : Type@{i}) (B : A -> Type@{i}), Type@{i})
       (A : Type@{i})
       (B : A -> Type@{i})
-      (P : W_type_Alpha t A B -> Type@{i})
+      (P : W_type_Alpha@{i} t A B -> Type@{i})
       (
         constructor_pair
-          : forall (a : A) (b : W_type_Beta t A B a), P (pair t A B a b)
+          : forall (a : A) (b : W_type_Beta@{i} t A B a), P (pair t A B a b)
       )
-    : forall x : W_type_Alpha t A B, P x
-    := Dependent_Sum.matching A (W_type_Beta t A B) P constructor_pair
+    : forall x : W_type_Alpha@{i} t A B, P x
+    :=
+      fun x : W_type_Alpha@{i} t A B =>
+        match x as x_ return P x_ with
+          wrap _ _ _ x_v
+            =>
+              Dependent_Sum.matching
+                A
+                (W_type_Beta t A B)
+                (
+                  fun x_v_ : Dependent_Sum A (W_type_Beta t A B) =>
+                    P (wrap t A B x_v_)
+                )
+                constructor_pair
+                x_v
+        end
 .
 (* from: originally defined by Hexirp *)
 
@@ -61,9 +88,9 @@ Definition
       (A : Type@{i})
       (B : A -> Type@{i})
       (P : Type@{i})
-      (constructor_pair : forall a : A, W_type_Beta t A B a -> P)
-    : W_type_Alpha t A B -> P
-    := matching t A B (fun x_ : W_type_Alpha t A B => P) constructor_pair
+      (constructor_pair : forall a : A, W_type_Beta@{i} t A B a -> P)
+    : W_type_Alpha@{i} t A B -> P
+    := matching t A B (fun x_ : W_type_Alpha@{i} t A B => P) constructor_pair
 .
 (* from: originally defined by Hexirp *)
 
