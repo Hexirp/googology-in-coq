f7ea0dae065a6a71fd870c566a3bcdf0db807236
Hexirp <Hexirp@users.noreply.github.com>
2022-03-06 17:51:40 +0900

    Update: W_type_Beta を
    newtype 方式を採用する。

--- libraries/theories/W_type_Beta.v
+++ libraries/theories/W_type_Beta.v
@@ -10,17 +10,48 @@ Import Googology_In_Coq.Function (Function).
 
 (** ライブラリを開きます。 *)
 
-Definition
+Inductive
   W_type_Beta@{i | }
       (t : forall A : Type@{i}, (A -> Type@{i}) -> Type@{i})
       (A : Type@{i})
       (B : A -> Type@{i})
-    : A -> Type@{i}
-    := fun a : A => Function (B a) (t A B)
+      (a : A)
+    : Type@{i}
+    := wrap : Function@{i} (B a) (t A B) -> W_type_Beta t A B a
 .
 (* from: originally defined by Hexirp *)
 
 (** ウ型のベータです。 *)
+
+Definition
+  unwrap@{i | }
+      (t : forall A : Type@{i}, (A -> Type@{i}) -> Type@{i})
+      (A : Type@{i})
+      (B : A -> Type@{i})
+      (a : A)
+    : W_type_Beta@{i} t A B a -> Function@{i} (B a) (t A B)
+    :=
+      fun x : W_type_Beta@{i} t A B a =>
+        match x with wrap _ _ _ _ x_v => x_v end
+.
+(* from: originally defined by Hexirp *)
+
+(** ウ型のベータです。 *)
+
+Definition
+  abstract@{i | }
+      (t : forall A : Type@{i}, (A -> Type@{i}) -> Type@{i})
+      (A : Type@{i})
+      (B : A -> Type@{i})
+      (a : A)
+    : (B a -> t A B) -> W_type_Beta@{i} t A B a
+    :=
+      fun x : B a -> t A B =>
+        wrap t A B a (Function.abstract (B a) (t A B) x)
+.
+(* from: originally defined by Hexirp *)
+
+(** ウ型のベータの抽象です。 *)
 
 Definition
   apply@{i | }
@@ -28,8 +59,10 @@ Definition
       (A : Type@{i})
       (B : A -> Type@{i})
       (a : A)
-    : W_type_Beta t A B a -> B a -> t A B
-    := Function.apply (B a) (t A B)
+    : W_type_Beta@{i} t A B a -> B a -> t A B
+    :=
+      fun x : W_type_Beta@{i} t A B a =>
+        Function.apply (B a) (t A B) (unwrap t A B a x)
 .
 (* from: originally defined by Hexirp *)
 
