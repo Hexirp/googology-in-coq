edb8979e810082ff34f7521f3b15995e0be0925e
Hexirp <Hexirp@users.noreply.github.com>
2022-03-07 21:12:33 +0900

    Update: Peano_Number_Tag を
    newtype 方式を導入する。

--- libraries/theories/Peano_Number_Tag.v
+++ libraries/theories/Peano_Number_Tag.v
@@ -12,17 +12,34 @@ Import Googology_In_Coq.Unit (Unit).
 
 (** ライブラリを開きます。 *)
 
-Definition Peano_Number_Tag@{i | } : Type@{i} := Sum@{i} Unit@{i} Unit@{i}.
+Inductive
+  Peano_Number_Tag@{i | } : Type@{i}
+    := wrap : Sum@{i} Unit@{i} Unit@{i} -> Peano_Number_Tag
+.
 (* from: originally defined by Hexirp *)
 
 (** 自然数の型のアルファです。 *)
 
-Definition zero@{i | } : Peano_Number_Tag@{i} := Sum.left Unit@{i} Unit@{i} Unit.unit.
+Inductive
+  unwrap@{i | } : Peano_Number_Tag@{i} -> Sum@{i} Unit@{i} Unit@{i}
+    := fun x : Peano_Number_Tag@{i} => match x with wrap x_v => x_v end
+.
+(* from: originally defined by Hexirp *)
+
+(** 自然数の型のアルファです。 *)
+
+Definition
+  zero@{i | } : Peano_Number_Tag@{i}
+    := wrap (Sum.left Unit@{i} Unit@{i} Unit.unit)
+.
 (* from: originally defined by Hexirp *)
 
 (** 自然数の型のアルファの第一構築子です。 *)
 
-Definition successor@{i | } : Peano_Number_Tag@{i} := Sum.right Unit@{i} Unit@{i} Unit.unit.
+Definition
+  successor@{i | } : Peano_Number_Tag@{i}
+    := wrap (Sum.right Unit@{i} Unit@{i} Unit.unit)
+.
 (* from: originally defined by Hexirp *)
 
 (** 自然数の型のアルファの第二構築子です。 *)
@@ -34,20 +51,34 @@ Definition
       (constructor_successor : P successor)
     : forall x : Peano_Number_Tag@{i}, P x
     :=
-      Sum.matching
-        Unit@{i}
-        Unit@{i}
-        P
-        (
-          Unit.matching
-            (fun x_ : Unit => P (Sum.left Unit@{i} Unit@{i} x_))
-            constructor_zero
-        )
-        (
-          Unit.matching
-            (fun x_ : Unit => P (Sum.right Unit@{i} Unit@{i} x_))
-            constructor_successor
-        )
+      fun x : Peano_Number_Tag@{i} =>
+        match x as x_ return x_ with
+          wrap x_v
+            =>
+              Sum.matching
+                Unit@{i}
+                Unit@{i}
+                (fun x_v_ : Sum@{i} Unit@{i} Unit@{i} => P (wrap x_v_))
+                (
+                  fun x_v_L : Unit@{i} =>
+                    Unit.matching
+                      (
+                        fun x_v_L_ : Unit@{i} =>
+                          P (wrap (Sum.left Unit@{i} Unit@{i} x_v_L_))
+                      )
+                      constructor_zero
+                )
+                (
+                  fun x_v_R : Unit@{i} =>
+                    Unit.matching
+                      (
+                        fun x_v_R_ : Unit =>
+                          P (wrap (Sum.right Unit@{i} Unit@{i} x_v_R_))
+                      )
+                      constructor_successor
+                )
+                x_v
+        end
 .
 (* from: originally defined by Hexirp *)
 
@@ -59,7 +90,11 @@ Definition
       (constructor_zero : P)
       (constructor_successor : P)
   : Peano_Number_Tag@{i} -> P
-  := matching (fun x_ : Peano_Number_Tag@{i} => P) constructor_zero constructor_successor
+  :=
+    matching
+      (fun x_ : Peano_Number_Tag@{i} => P)
+      constructor_zero
+      constructor_successor
 .
 (* from: originally defined by Hexirp *)
 
