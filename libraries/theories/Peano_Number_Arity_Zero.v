6089ef43488326da6f220abdfbae57887cfb280f
Hexirp <Hexirp@users.noreply.github.com>
2022-03-07 21:59:57 +0900

    Update: Peano_Number_Arity_Zero を
    newtype 方式を採用する。

--- libraries/theories/Peano_Number_Arity_Zero.v
+++ libraries/theories/Peano_Number_Arity_Zero.v
@@ -10,14 +10,30 @@ Import Googology_In_Coq.Void (Void).
 
 (** ライブラリを開きます。 *)
 
-Definition Peano_Number_Arity_Zero@{i | } : Type@{i} := Void@{i}.
+Inductive
+  Peano_Number_Arity_Zero@{i | } : Type@{i}
+    := wrap : Void@{i} -> Peano_Number_Arity_Zero
+.
 (* from: originally defined by Hexirp *)
 
 (** 自然数の型のベータの [zero] です。 *)
 
 Definition
-  matching@{i | } (P : Peano_Number_Arity_Zero@{i} -> Type@{i}) : forall x : Peano_Number_Arity_Zero@{i}, P x
-    := Void.matching P
+  unwrap@{i | } : Peano_Number_Arity_Zero@{i} -> Void@{i}
+    := fun x : Peano_Number_Arity_Zero@{i} => match x with wrap x_v => x_v end
+.
+(* from: originally defined by Hexirp *)
+
+(** 自然数の型のベータの [zero] です。 *)
+
+Definition
+  matching@{i | } (P : Peano_Number_Arity_Zero@{i} -> Type@{i})
+    : forall x : Peano_Number_Arity_Zero@{i}, P x
+    :=
+      fun x : Peano_Number_Arity_Zero@{i} =>
+        match x as x_ return P x_ with
+          wrap x_v => Void.matching (fun x_v_ : Void@{i} => P (wrap x_v_)) x_v
+        end
 .
 (* from: originally defined by Hexirp *)
 
