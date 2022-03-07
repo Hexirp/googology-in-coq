fcc45e3b19b4fd54c79d16cc5a88ce819cd2131e
Hexirp <Hexirp@users.noreply.github.com>
2022-03-08 01:31:44 +0900

    Update: Peano_Number_Arity を
    newtype 方式を採用する。

--- libraries/theories/Peano_Number_Arity.v
+++ libraries/theories/Peano_Number_Arity.v
@@ -16,16 +16,50 @@ Import Googology_In_Coq.Peano_Number_Arity_Successor (Peano_Number_Arity_Success
 
 (** ライブラリを開きます。 *)
 
-Definition
-  Peano_Number_Arity@{i s_i | i < s_i} : Peano_Number_Tag@{i} -> Type@{i}
-    := Peano_Number_Tag.matching_nodep Universe@{i s_i} Peano_Number_Arity_Zero@{i} Peano_Number_Arity_Successor@{i}
+Inductive
+  Peano_Number_Arity@{i s_i | i < s_i} (x : Peano_Number_Tag@{i}) : Type@{i}
+    :=
+      wrap
+        :
+            Universe.unwrap
+              (
+                Peano_Number_Tag.matching_nodep
+                  Universe@{i s_i}
+                  (Universe.wrap Peano_Number_Arity_Zero@{i})
+                  (Universe.wrap Peano_Number_Arity_Successor@{i})
+              )
+              x
+          ->
+            Peano_Number_Arity
+.
+(* from: originally defined by Hexirp *)
+
+(** 自然数の型のベータです。 *)
+
+Inductive
+  unwrap@{i s_i | i < s_i} (x : Peano_Number_Tag@{i})
+    :
+        Peano_Number_Arity@{i s_i}
+      ->
+        Universe.unwrap
+          (
+            Peano_Number_Tag.matching_nodep
+              Universe@{i s_i}
+              (Universe.wrap Peano_Number_Arity_Zero@{i})
+              (Universe.wrap Peano_Number_Arity_Successor@{i})
+              x
+          )
+    :=
+      fun x : Peano_Number_Arity@{i s_i} =>
+        match x with wrap _ x_v => x_v end
 .
 (* from: originally defined by Hexirp *)
 
 (** 自然数の型のベータです。 *)
 
 Definition
-  zero@{i s_i | i < s_i} (A : Type@{i}) : Peano_Number_Arity Peano_Number_Tag.zero -> A
+  zero@{i s_i | i < s_i} (A : Type@{i})
+    : Peano_Number_Arity@{i s_i} Peano_Number_Tag.zero -> A
     := Peano_Number_Arity_Zero.matching_nodep A
 .
 (* from: originally defined by Hexirp *)
@@ -33,7 +67,8 @@ Definition
 (** 自然数の型のベータの [zero] です。 *)
 
 Definition
-  successor@{i s_i | i < s_i} (A : Type@{i}) (x_p : A) : Peano_Number_Arity Peano_Number_Tag.successor -> A
+  successor@{i s_i | i < s_i} (A : Type@{i}) (x_p : A)
+    : Peano_Number_Arity@{i s_i} Peano_Number_Tag.successor -> A
     := Peano_Number_Arity_Successor.matching_nodep A x_p
 .
 (* from: originally defined by Hexirp *)
