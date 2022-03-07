92cd83e6bbdd50e0d1b5edae335e3016f39d3151
Hexirp <Hexirp@users.noreply.github.com>
2022-03-07 22:15:37 +0900

    Update: Peano_Number_Arity_Successor を
    newtype 方式を採用する。

--- libraries/theories/Peano_Number_Arity_Successor.v
+++ libraries/theories/Peano_Number_Arity_Successor.v
@@ -10,35 +10,65 @@ Import Googology_In_Coq.Unit (Unit).
 
 (** ライブラリを開きます。 *)
 
-Definition Peano_Number_Arity_Successor@{i | } : Type@{i} := Unit@{i}.
+Inductive
+  Peano_Number_Arity_Successor@{i | } : Type@{i}
+    := wrap : Unit@{i} -> Peano_Number_Arity_Successor
+.
 (* from: originally defined by Hexirp *)
 
 (** 自然数の型のベータの [succ] です。 *)
 
-Definition unit@{i | } : Unit@{i} := Unit.unit.
+Inductive
+  unwrap{i | } : Peano_Number_Arity_Successor@{i} -> Unit@{i}
+    :=
+      fun x : Peano_Number_Arity_Successor@{i} =>
+        match x with wrap x_v => x_v end
+.
+(* from: originally defined by Hexirp *)
+
+(** 自然数の型のベータの [succ] です。 *)
+
+Definition unit@{i | } : Unit@{i} := wrap Unit.unit.
 (* from: originally defined by Hexirp *)
 
 (** 自然数の型のベータの [succ] の構築子です。 *)
 
 Definition
-  matching@{i | } (P : Peano_Number_Arity_Successor@{i} -> Type@{i}) (constructor_unit : P unit)
+  matching@{i | }
+      (P : Peano_Number_Arity_Successor@{i} -> Type@{i})
+      (constructor_unit : P unit)
     : forall x : Peano_Number_Arity_Successor@{i}, P x
-    := Unit.matching P constructor_unit
+    :=
+      fun x : Peano_Number_Arity_Successor@{i} =>
+        match x as x_ return P x_ with
+          wrap x_v
+            =>
+              Unit.matching
+                (fun x_v_ : Unit@{i} => P (wrap x_v_))
+                constructor_unit
+                x_v_
+        end
 .
 (* from: originally defined by Hexirp *)
 
 (** 場合分けです。 *)
 
 Definition
-  matching_nodep@{i | } (P : Type@{i}) (constructor_unit : P) : Peano_Number_Arity_Successor@{i} -> P
-    := matching (fun x_ : Peano_Number_Arity_Successor@{i} => P) constructor_unit
+  matching_nodep@{i | } (P : Type@{i}) (constructor_unit : P)
+    : Peano_Number_Arity_Successor@{i} -> P
+    :=
+      matching
+        (fun x_ : Peano_Number_Arity_Successor@{i} => P)
+        constructor_unit
 .
 (* from: originally defined by Hexirp *)
 
 (** 場合分けです。 *)
 
 Definition
-  comatching_nodep@{i | } (P : Type@{i}) : P -> Peano_Number_Arity_Successor@{i} := fun x : P => unit
+  comatching_nodep@{i | } (P : Type@{i})
+    : P -> Peano_Number_Arity_Successor@{i}
+    := fun x : P => unit
 .
 (* from: originally defined by Hexirp *)
 
