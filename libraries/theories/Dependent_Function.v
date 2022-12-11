e7fc4db4de9b20f8424c128b3aa129d33fc2f46c
Hexirp Hexirp@users.noreply.github.com <Hexirp@users.noreply.github.com>
2022-12-12 02:26:00 +0900

    Update: 記述を削り、できるだけシンプルに保つ

--- libraries/theories/Dependent_Function.v
+++ libraries/theories/Dependent_Function.v
@@ -8,100 +8,12 @@ Import Googology_In_Coq.Base.
 
 (** ライブラリを開きます。 *)
 
-Inductive Dependent_Function@{ i | } ( A : Type@{ i } ) ( B : A -> Type@{ i } ) : Type@{ i } := wrap : ( forall x : A, B x ) -> Dependent_Function A B.
+Definition Dependent_Function@{ i | } ( A : Type@{ i } ) ( B : A -> Type@{ i } ) : Type@{ i } := forall x : A, B x.
 (* from: originally defined by Hexirp *)
 
 (** 依存関数型です。 *)
 
-Definition matching@{ i | } ( A : Type@{ i } ) ( B : A -> Type@{ i } ) ( E : Dependent_Function@{ i } A B -> Dependent_Function@{ i } A B -> Type@{i} ) ( P : Type@{ i } ) ( d : P -> Dependent_Function@{ i } ) ( cw : ( forall x : A, B x ) -> P ) ( ew : forall x : Dependent_Function@{ i } A B, E ( d ( cw x ) ) x ) : Dependent_Function@{ i } A B -> exists x : P, d x =
-
-Definition
-  unwrap@{i | } (A : Type@{i}) (B : A -> Type@{i})
-    : Dependent_Function@{i} A B -> forall x : A, B x
-    :=
-      fun x : Dependent_Function@{i} A B =>
-        match x with wrap _ _ x_v => x_v end
-.
+Definition map_Dependent_Function@{ i | } ( A : Type@{ i } ) ( B : A -> Type@{ i } ) ( C : Type@{ i } ) ( D : C -> Type@{ i } ) ( f : C -> A ) ( g : forall x : C, B ( f x ) -> D x ) ( x : Dependent_Function A B ) : Dependent_Function C D := fun y : C => g y ( x ( f y ) ).
 (* from: originally defined by Hexirp *)
 
-(** 依存関数型です。 *)
-
-Definition
-  abstract@{i | } (A : Type@{i}) (B : A -> Type@{i})
-    : (forall x : A, B x) -> Dependent_Function@{i} A B
-    := wrap A B
-.
-(* from: originally defined by Hexirp *)
-
-(** 抽象です。ラムダ抽象です。 *)
-
-Definition
-  matching@{i | }
-      (A : Type@{i})
-      (B : A -> Type@{i})
-      (P : Dependent_Function@{i} A B -> Type@{i})
-      (
-        constructor_abstract
-          : forall x_v : forall x : A, B x, P (abstract A B x_v)
-      )
-    : forall x : Dependent_Function@{i} A B, P x
-    :=
-      fun x : Dependent_Function@{i} A B =>
-        match x as x_ return P x_ with
-          wrap _ _ x_v => constructor_abstract x_v
-        end
-.
-(* from: originally defined by Hexirp *)
-
-(** 場合分けです。 *)
-
-Definition
-  matching_nodep@{i | }
-      (A : Type@{i})
-      (B : A -> Type@{i})
-      (P : Type@{i})
-      (constructor_abstract : (forall x : A, B x) -> P)
-    : Dependent_Function@{i} A B -> P
-    :=
-      matching
-        A
-        B
-        (fun x_ : Dependent_Function@{i} A B => P)
-        constructor_abstract
-.
-(* from: originally defined by Hexirp *)
-
-(** 場合分けです。 *)
-
-Definition
-  apply@{i | } (A : Type@{i}) (B : A -> Type@{i})
-    : Dependent_Function@{i} A B -> forall x : A, B x
-    :=
-      matching_nodep
-        A
-        B
-        (forall x : A, B x)
-        (fun x_v : forall x : A, B x => x_v)
-.
-(* from: originally defined by Hexirp *)
-
-(** 適用です。 *)
-
-(** [abstract] は構築子です。 [apply] は分解子です。甲を [abstract] で構築したものを [apply] で分解したものは甲と同じというのが β-変換です。甲を [apply] で分解したものを [abstract] で構築したものは甲と同じというのが η-変換です。「全てのイに対して甲をイに適用したものと乙をイに適用したものが同じである」であれば甲を [abstract] で構築したものと乙を [abstract] で構築したものが同じというのが判断同値の関数外延性です。甲と乙が同じかつイとロが同じであれば甲を [apply] で分解したものをイに適用したものと乙を [apply] で分解したものをロに適用したものは同じであるというのがホニャララです。 *)
-
-Definition
-  map@{i | }
-      (A : Type@{i})
-      (B : A -> Type@{i})
-      (C : Type@{i})
-      (D : C -> Type@{i})
-      (f : C -> A)
-      (g : forall x : C, B (f x) -> D x)
-    : Dependent_Function A B -> Dependent_Function C D
-    :=
-      fun x : Dependent_Function A B =>
-        abstract C D (fun y : C => g y (apply A B x (f y)))
-.
-(* from: originally defined by Hexirp *)
-
-(** 依存関数型の写像です。 *)
+(** 依存関数型の上の写像です。 *)
