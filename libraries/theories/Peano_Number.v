c0111b01df883dc04bd937ab3318245839c14dbf
Hexirp Hexirp@users.noreply.github.com <Hexirp@users.noreply.github.com>
2022-12-29 18:29:17 +0900

    Add: 自然数型の場合分けを追加する

--- libraries/theories/Peano_Number.v
+++ libraries/theories/Peano_Number.v
@@ -14,3 +14,18 @@ Inductive Peano_Number@{ i | } : Type@{ i } := zero_Peano_Number : Peano_Number 
 (* from: originally defined by Hexirp *)
 
 (** 自然数型です。 *)
+
+Definition matching_Peano_Number@{ i j | } ( P : Type@{ j } ) ( cz : P ) ( cs : Peano_Number@{ i } -> P ) ( x : Peano_Number@{ i } ) : P := match x with zero_Peano_Number => cz | succ_Peano_Number xp => cs xp end.
+(* from: originally defined by Hexirp *)
+
+(** 自然数型の場合分けです。 *)
+
+Definition identity_matching_Peano_Number@{ i j | } ( P : Type@{ j } ) ( display : P -> Peano_Number@{ i } ) ( cz : P ) ( icz : Path Peano_Number@{ i } ( display cz ) zero_Peano_Number ) ( cs : Peano_Number@{ i } -> P ) ( ics : forall xp : Peano_Number@{ i }, Path Peano_Number@{ i } ( display ( cs xp ) ) ( succ_Peano_Number xp ) ) ( x : Peano_Number@{ i } ) : Path Peano_Number@{ i } ( display ( matching_Peano_Number P cz cs x ) ) x := match x as x_ return Path Peano_Number@{ i } ( display ( matching_Peano_Number P cz cs x_ ) ) x_ with zero_Peano_Number => icz | succ_Peano_Number xp => ics xp end.
+(* from: originally defined by Hexirp *)
+
+(** 自然数型の場合分けの恒等式です。 *)
+
+Definition dependent_matching_Peano_Number@{ i j | } ( P : Peano_Number@{ i } -> Type@{ j } ) ( cz : P zero_Peano_Number ) ( cs : forall xp : Peano_Number@{ i }, P ( succ_Peano_Number xp ) ) ( x : Peano_Number@{ i } ) : P x := match x as x_ return P x_ with zero_Peano_Number => cz | succ_Peano_Number xp => cs xp end.
+(* from: originally defined by Hexirp *)
+
+(** 自然数型の場合分けです。 *)
