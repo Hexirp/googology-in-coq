96d710459fc24f419807f162f0036b7dd8242dff
Hexirp Hexirp@users.noreply.github.com <Hexirp@users.noreply.github.com>
2023-01-02 16:55:27 +0900

    Add: identity_recursion_Peano_Number を定義する

--- libraries/theories/Peano_Number.v
+++ libraries/theories/Peano_Number.v
@@ -30,14 +30,25 @@ Definition dependent_matching_Peano_Number@{ i j | } ( P : Peano_Number@{ i } ->
 
 (** 自然数型の場合分けです。 *)
 
-Definition recursion_Peano_Number@{ i j | } ( P : Type@{ j } ) ( cz : P ) ( cs : P -> P ) ( x : Peano_Number@{ i } ) : P := ( fix rec ( x_ : Peano_Number@{ i } ) { struct x_ } : P := matching_Peano_Number P cz ( fun xp : Peano_Number@{ i } => cs ( rec xp ) ) x_ ) x.
+Definition recursion_Peano_Number@{ i j | } ( P : Type@{ j } ) ( cz : P ) ( cs : P -> P ) ( x : Peano_Number@{ i } ) : P := ( fix rec ( x_ : Peano_Number@{ i } ) { struct x_ } : P := match x_ with zero_Peano_Number => cz | succ_Peano_Number xp => cs ( rec xp ) end ) x.
 (* from: originally defined by Hexirp *)
 
 (** 自然数型の再帰です。 *)
 
 Definition identity_recursion_Peano_Number@{ i j | } ( P : Type@{ j } ) ( display : P -> Peano_Number@{ i } ) ( cz : P ) ( icz : Path Peano_Number@{ i } ( display cz ) zero_Peano_Number ) ( cs : P -> P ) ( ics : forall ( xp : Peano_Number@{ i } ) ( rp : P ), Path Peano_Number@{ i } ( display rp ) xp -> Path Peano_Number@{ i } ( display ( cs rp ) ) ( succ_Peano_Number xp ) ) ( x : Peano_Number@{ i } ) : Path Peano_Number@{ i } ( display ( recursion_Peano_Number P cz cs x ) ) x.
 Proof.
-  exact _.
+  refine ( _ x ).
+  refine ( fix rec ( x_ : Peano_Number@{ i } ) { struct x_ } : Path Peano_Number@{ i } ( display ( recursion_Peano_Number P cz cs x_ ) ) x_ := _ ).
+  refine ( match x_ as x__ return Path Peano_Number@{ i } ( display ( recursion_Peano_Number P cz cs x__ ) ) x__ with zero_Peano_Number => _ | succ_Peano_Number xp => _ end ).
+  -
+    change ( Path Peano_Number@{ i } ( display ( recursion_Peano_Number P cz cs zero_Peano_Number ) ) zero_Peano_Number ).
+    change ( Path Peano_Number@{ i } ( display cz ) zero_Peano_Number ).
+    exact icz.
+  -
+    change ( Path Peano_Number@{ i } ( display ( recursion_Peano_Number P cz cs ( succ_Peano_Number xp ) ) ) ( succ_Peano_Number xp ) ).
+    change ( Path Peano_Number@{ i } ( display ( cs ( recursion_Peano_Number P cz cs xp ) ) ) ( succ_Peano_Number xp ) ).
+    refine ( ics xp ( recursion_Peano_Number P cz cs xp ) _ ).
+    exact ( rec xp ).
 Defined.
 (* from: originally defined by Hexirp *)
 
