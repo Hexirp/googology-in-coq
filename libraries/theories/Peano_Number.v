f0f8356cbe7f9af7336c8a3f29bda7441af7e46e
Hexirp Hexirp@users.noreply.github.com <Hexirp@users.noreply.github.com>
2023-01-13 00:04:10 +0900

    Update: add_right_unit_Peano_Number を追加する

--- libraries/theories/Peano_Number.v
+++ libraries/theories/Peano_Number.v
@@ -84,9 +84,9 @@ Definition add_Peano_Number@{ i | } ( m : Peano_Number@{ i } ) ( n : Peano_Numbe
 
 (** 加算です。 *)
 
-Definition add_left_unit_Peano_Number@{ i | } ( m : Peano_Number@{ i } ) : Path Peano_Number@{ i } ( add_Peano_Number zero_Peano_Number m ) m.
+Definition add_left_unit_Peano_Number@{ i | } ( n : Peano_Number@{ i } ) : Path Peano_Number@{ i } ( add_Peano_Number zero_Peano_Number n ) n.
 Proof.
-  refine ( dependent_recursion_Peano_Number ( fun m_ : Peano_Number@{ i } => Path Peano_Number@{ i } ( add_Peano_Number zero_Peano_Number m_ ) m_ ) _ _ m ).
+  refine ( dependent_recursion_Peano_Number ( fun n_ : Peano_Number@{ i } => Path Peano_Number@{ i } ( add_Peano_Number zero_Peano_Number n_ ) n_ ) _ _ n ).
   -
     change ( Path Peano_Number@{ i } ( add_Peano_Number zero_Peano_Number zero_Peano_Number ) zero_Peano_Number ).
     change ( Path Peano_Number@{ i } zero_Peano_Number zero_Peano_Number ).
@@ -100,6 +100,11 @@ Defined.
 (* from: originally defined by Hexirp *)
 
 (** 加算の左単位元法則です。 *)
+
+Definition add_right_unit_Peano_Number@{ i | } ( m : Peano_Number@{ i } ) : Path Peano_Number@{ i } ( add_Peano_Number m zero_Peano_Number ) m := id_Path Peano_Number@{ i } m.
+(* from: originally defined by Hexirp *)
+
+(** 加算の右単位元法則です。 *)
 
 Definition mul_Peano_Number@{ i | } ( m : Peano_Number@{ i } ) ( n : Peano_Number@{ i } ) : Peano_Number@{ i } := recursion_Peano_Number Peano_Number@{ i } zero_Peano_Number ( fun rp : Peano_Number@{ i } => add_Peano_Number m rp ) n.
 (* from: originally defined by Hexirp *)
