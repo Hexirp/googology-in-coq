ee122de4945d3d83f20b583f2a1dc6da2a0831bb
Hexirp Hexirp@users.noreply.github.com <Hexirp@users.noreply.github.com>
2024-08-12 21:48:28 +0900

    Chore: 新しい見た目の規則に合わせる

--- libraries/theories/Base.v
+++ libraries/theories/Base.v
@@ -2,32 +2,32 @@
 
 (** [ltac_plugin] を読み込みます。 *)
 
-Declare ML Module "ltac_plugin".
+Declare ML Module "ltac_plugin" .
 
 (** 対話証明の形式を [Classic] にします。 *)
 
-Global Set Default Proof Mode "Classic".
+Global Set Default Proof Mode "Classic" .
 
 (** 目標を選ぶ方法を [!] にします。 *)
 
-Global Set Default Goal Selector "!".
+Global Set Default Goal Selector "!" .
 
 (** [Elimination Schemes] を無効にします。 *)
 
-Global Unset Elimination Schemes.
+Global Unset Elimination Schemes .
 
 (** [Universe Polymorphism] を有効にします。 *)
 
-Global Set Universe Polymorphism.
+Global Set Universe Polymorphism .
 
 (** [Polymorphic Inductive Cumulativity] を有効にします。 *)
 
-Global Set Polymorphic Inductive Cumulativity.
+Global Set Polymorphic Inductive Cumulativity .
 
 (** 宇宙の詳細を表示するようにします。 *)
 
-Global Set Printing Universes.
+Global Set Printing Universes .
 
 (** [forall _ : x, y] の糖衣構文として [x -> y] を定義します。 << https://github.com/coq/coq/blob/aaa8c94362b9159b1fa00baff8cd50cdc2972c7f/theories/Init/Notations.v#L15 >> を参照しています。 *)
 
-Notation "x -> y" := ( forall _ : x, y ) ( at level 99, right associativity, y at level 200 ).
+Notation "x -> y" := ( forall _ : x , y ) ( at level 99 , right associativity , y at level 200 ) .
